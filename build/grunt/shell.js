03acd13c0f6dcb7903b7f2b15140c693723337ab
Walter Rumsby <walter.rumsby@xero.com>
2015-09-03 11:17:06 +1200

    Slashy and nicey.

--- build/grunt/shell.js
+++ build/grunt/shell.js
@@ -1,9 +1,17 @@
+var slash = require('slash');
+
 module.exports = {
   install: {
     command: 'bundle install --frozen --path vendor/bundle'
   },
 
   styleguide: {
-      command: './node_modules/.bin/kss-node -c kss-config.json'
+      command: function () {
+        'use strict';
+
+        var cmd = slash('./node_modules/.bin/kss-node');
+
+        return cmd + ' -c kss-config.json';
+      }
   }
 };

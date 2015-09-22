618945678a4f703d97660774a2544fdee21977c9
Abrahm Coffman <abrahm.coffman@xero.com>
2015-09-22 11:42:48 -0700

    Added the ability to pass flags to grunt tasks via nopt. Added support for the livereload flag for the watch task, and updated the the docs

--- build/grunt/watch.js
+++ build/grunt/watch.js
@@ -1,4 +1,22 @@
-module.exports = {
-	files: ['**/*.scss'],
-	tasks: ['lint', 'build', 'kss']
+module.exports = function(grunt, opts) {
+	var livereload;
+
+	if(opts.livereload === 1) {
+		livereload = true;
+	}
+	else if(opts.livereload === 0) {
+		livereload = false;
+	}
+	// allows for setting an explicit port, otherwise defaults to 35729
+	else {
+		livereload = opts.livereload;
+	}
+
+	return {
+		options: {
+			livereload: livereload
+		},
+		files: ['**/*.scss'],
+		tasks: ['lint', 'build', 'kss']
+	}
 };

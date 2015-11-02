c023c05d1c46227faa2bfe0dafcf882b230565b3
James Macfie <james.macfie@xero.com>
2015-11-03 12:27:15 +1300

    Use reporting-component for all components

--- src/XUIButtonCaret.js
+++ src/XUIButtonCaret.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Component from 'reporting-base-component';
 
-export default class XUIButtonCaret extends React.Component {
+export default class XUIButtonCaret extends Component {
+	constructor(props, context) {
+		super(props, context);
+	}
+	
 	render() {
 		return (
 				<span className='xui-button--caret'></span>
--- src/XUIButtonGroup.js
+++ src/XUIButtonGroup.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Component from 'reporting-base-component';
 
-export default class XUIButtonGroup extends React.Component {
+export default class XUIButtonGroup extends Component {
+	constructor(props, context) {
+		super(props, context);
+	}
+	
 	render() {
 		return (
 			<div className='xui-buttongroup'>{this.props.children}</div>

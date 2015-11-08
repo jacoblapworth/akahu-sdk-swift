d697e3557d14b7f8949098f3cb46537ca7d30d32
Tony Anderson <tony.anderson@xero.com>
2015-11-09 10:45:54 +1300

    Added optional parameters (class name and style)

--- src/XUILoader.js
+++ src/XUILoader.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Component from 'xui-base-component';
 
+const propTypes = {
+	className: React.PropTypes.string,
+	customStyle: React.PropTypes.string
+};
+
 export default class XUILoader extends Component {
 	render() {
 		return (
-			<div className={'xui-loader'}>
+			<div className={'xui-loader ' + this.props.className} style={this.props.customStyle}>
 				<div className={'xui-loader--dot'}></div>
 				<div className={'xui-loader--dot'}></div>
 				<div className={'xui-loader--dot'}></div>
@@ -12,3 +17,4 @@ export default class XUILoader extends Component {
 		);
 	}
 }
+XUILoader.propTypes = propTypes;
--- test/ui/test.js
+++ test/ui/test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import XUILoader from '../../src/Loader.js';
+import XUILoader from '../../src/XUILoader.js';
 import '../../src/scss/_loader.scss';
 
 (function() {
+	const optionalStyle = {backgroundColor: '#f1f4f5'};
 	React.render(
-		<XUILoader/>, document.getElementById('app')
+
+		<XUILoader className='custom-class' customStyle={optionalStyle} />, document.getElementById('app')
 	);
 })();

0ab73db74ca6fdcc129f257435a1a2e4d0505423
Walter Rumsby <walter.rumsby@xero.com>
2015-11-13 10:25:22 +1300

    Use kss-node directly rather than going through grunt-kss

--- build/grunt/kss.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	options: {
-		config: 'kss-config.json'
-	},
-	styleguide: {
-	}
-};
--- build/grunt/shell.js
+++ build/grunt/shell.js
@@ -1,5 +1,8 @@
 module.exports = {
   install: {
     command: 'bundle install --frozen --path vendor/bundle'
+  },
+  kss: {
+    command: './node_modules/.bin/kss-node --config kss-config.json'
   }
 };

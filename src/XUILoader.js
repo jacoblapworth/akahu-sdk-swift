3074a09b853f7d58c52ac6d4edd73788e168c6f8
Tony Anderson <tony.anderson@xero.com>
2015-11-25 14:57:43 +1300

    Improvemnts based off issues #2, #8, #9

--- src/XUILoader.js
+++ src/XUILoader.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Component from 'xui-base-component';
+import cn from 'classnames';
 
 const propTypes = {
 	className: React.PropTypes.string,
-	customStyle: React.PropTypes.string
+	customStyle: React.PropTypes.object
 };
 
 export default class XUILoader extends Component {
 	render() {
+		const classNames = cn('xui-loader', this.props.className);
 		return (
-			<div className={'xui-loader ' + this.props.className} style={this.props.customStyle}>
+			<div className={classNames} style={this.props.customStyle}>
 				<div className='xui-loader--dot'></div>
 				<div className='xui-loader--dot'></div>
 				<div className='xui-loader--dot'></div>
--- test/ui/test.js
+++ test/ui/test.js
@@ -4,7 +4,6 @@ import XUILoader from '../../src/XUILoader.js';
 (function() {
 	const optionalStyle = {backgroundColor: '#f1f4f5'};
 	React.render(
-
 		<XUILoader className='custom-class' customStyle={optionalStyle} />, document.getElementById('app')
 	);
 })();

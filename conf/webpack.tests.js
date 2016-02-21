d394d2a168b24ad738ad2bd7aca96cae9d170565
Tim Redmond <tim.redmond@xero.com>
2016-02-22 09:51:05 +1300

    Getting Ready for UXE endorsement

--- conf/webpack.tests.js
+++ conf/webpack.tests.js
@@ -1,6 +1,3 @@
-require('core-js/es5');
-require('es6-shim');
-
 import 'babel-core/external-helpers.js';
 
 // Replace ./src/js with the directory of your application code and
--- test/ui/test.js
+++ test/ui/test.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import XUISwitch from '../../src/XUISwitch.js';
 
+const SwitchModel = (checked, disabled) => ({disabled, checked});
+
 class FakeApp extends Component {
 
 	constructor() {
@@ -11,27 +13,71 @@ class FakeApp extends Component {
 
 		this.state = {
 			// Sets default disabled for this switch example
-			switchEnabled : false
+			switch1 : new SwitchModel(true, false),
+			switch2 : new SwitchModel(false, false),
+			switch3 : new SwitchModel(true, true),
+			switch4 : new SwitchModel(false, false)
 		};
 	}
 
-	handleSwitch(event) {
-		console.log('Switch triggered', event, this.state.switchEnabled);
+	handleSwitch(thisSwitch, event) {
 
-		const switchEnabled = !this.state.switchEnabled;
+		const checked = !this.state[thisSwitch].checked;
 
 		this.setState({
-			switchEnabled
+			[thisSwitch] : {
+				checked
+			}
+		}, () => {
+			if (thisSwitch === 'switch4') {
+
+				const newState = {...this.state};
+
+				newState.switch1.disabled = checked;
+				newState.switch2.disabled = checked;
+				newState.switch3.disabled = checked;
+
+				this.setState(newState);
+			}
 		});
 	}
 
 	render() {
+
+		console.log(this.state);
+		const { switch1, switch2, switch3, switch4 } = this.state;
+
 		return (
-			<XUISwitch
-				isEnabled={this.state.switchEnabled}
-				onChange={this.handleSwitch.bind(this)}
-				value='someValue'
-				name='someName'></XUISwitch>
+
+			<div>
+				<XUISwitch
+					checked={switch1.checked}
+					disabled={switch1.disabled}
+					onChange={this.handleSwitch.bind(this, 'switch1')}
+					value='someValue'
+					name='someName'></XUISwitch>
+				<XUISwitch
+					checked={switch2.checked}
+					disabled={switch2.disabled}
+					onChange={this.handleSwitch.bind(this, 'switch2')}
+					value='someValue'
+					name='someName'></XUISwitch>
+				<XUISwitch
+					checked={switch3.checked}
+					disabled={switch3.disabled}
+					onChange={this.handleSwitch.bind(this, 'switch3')}
+					value='someValue'
+					name='someName'></XUISwitch>
+
+				<h1>Disable all the things</h1>
+
+				<XUISwitch
+					checked={switch4.checked}
+					disabled={switch4.disabled}
+					onChange={this.handleSwitch.bind(this, 'switch4')}
+					value='someValue'
+					name='someName'></XUISwitch>
+			</div>
 		);
 	}
 }

f8852d04713128605ecf9d24d562efdcc0354bf5
Alan R. Soares <alanrsoars@gmail.com>
2016-04-05 13:19:15 +1200

    remove unnecessary double indentation

--- test/ui/test.js
+++ test/ui/test.js
@@ -11,74 +11,74 @@ import XUIButton, {XUIButtonGroup, XUIButtonCaret} from '../../index.js';
 
 	const examples = (
 		<div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler}>Default button</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler} variant="primary">Primary button</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler} variant="create">Create button</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler} variant="negative">Negative button</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler} isDisabled={true}>Disabled button</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-				<XUIButton onClick={clickHandler} variant="create">A dropdown button<XUIButtonCaret /></XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButtonGroup>
-						<XUIButton onClick={clickHandler} isGrouped={true}>Grouped Button One</XUIButton>
-						<XUIButton onClick={clickHandler} isGrouped={true}>Grouped Button Two</XUIButton>
-						<XUIButton onClick={clickHandler} isGrouped={true}>Grouped Button Three</XUIButton>
-					</XUIButtonGroup>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler}>Click me for an alert</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler} size="small">Small button</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler} size="full-width">Full width button</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler} type="link">Simple anchor tag with no href</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton onClick={clickHandler} type="link" variant="negative">Simple negative anchor tag with no href</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton
-						type="link"
-						href="http://go.xero.com">Simple anchor tag with href</XUIButton>
-				</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler}>Default button</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler} variant="primary">Primary button</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler} variant="create">Create button</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler} variant="negative">Negative button</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler} isDisabled={true}>Disabled button</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+			<XUIButton onClick={clickHandler} variant="create">A dropdown button<XUIButtonCaret /></XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButtonGroup>
+					<XUIButton onClick={clickHandler} isGrouped={true}>Grouped Button One</XUIButton>
+					<XUIButton onClick={clickHandler} isGrouped={true}>Grouped Button Two</XUIButton>
+					<XUIButton onClick={clickHandler} isGrouped={true}>Grouped Button Three</XUIButton>
+				</XUIButtonGroup>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler}>Click me for an alert</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler} size="small">Small button</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler} size="full-width">Full width button</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler} type="link">Simple anchor tag with no href</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton onClick={clickHandler} type="link" variant="negative">Simple negative anchor tag with no href</XUIButton>
+			</div>
 			<div className="testButtonContainer">
 				<XUIButton
 					type="link"
-					isDisabled={true}
-					href="http://go.xero.com">Disabled link button</XUIButton>
+					href="http://go.xero.com">Simple anchor tag with href</XUIButton>
 			</div>
-				<div className="testButtonContainer">
-					<XUIButton
-							type="link"
-							href="http://go.xero.com"
-							target="_blank">Anchor tag with `_blank` target</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton
-						onClick={clickHandler}
-						className="fun-times">Button with non-XUI modifier class</XUIButton>
-				</div>
-				<div className="testButtonContainer">
-					<XUIButton
-						onClick={clickHandler}
-						qaHook="buttonQAHook">Button with a QA hook class</XUIButton>
-				</div>
+		<div className="testButtonContainer">
+			<XUIButton
+				type="link"
+				isDisabled={true}
+				href="http://go.xero.com">Disabled link button</XUIButton>
+		</div>
+			<div className="testButtonContainer">
+				<XUIButton
+					type="link"
+					href="http://go.xero.com"
+					target="_blank">Anchor tag with `_blank` target</XUIButton>
 			</div>
+			<div className="testButtonContainer">
+				<XUIButton
+					onClick={clickHandler}
+					className="fun-times">Button with non-XUI modifier class</XUIButton>
+			</div>
+			<div className="testButtonContainer">
+				<XUIButton
+					onClick={clickHandler}
+					qaHook="buttonQAHook">Button with a QA hook class</XUIButton>
+			</div>
+		</div>
 	);
 
 	ReactDOM.render(examples, document.getElementById('app'));

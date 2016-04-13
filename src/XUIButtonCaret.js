dbd7a9f08222dc00dccc419757f016ef3d57eeba
Alan R. Soares <alanrsoars@gmail.com>
2016-04-13 13:38:25 +1200

    remove unnecessary default props on XUIButtonCaret

--- src/XUIButtonCaret.js
+++ src/XUIButtonCaret.js
@@ -8,16 +8,10 @@ const PropTypes = {
 	isSelect: React.PropTypes.bool
 };
 
-const DefaultProps = {
-	isSelect: false,
-	pureRender: true
-};
-
 const XUIButtonCaret = ({ isSelect }) => (
 	<span className={isSelect ? 'xui-select--caret' : ButtonClasses.CARET}></span>
 );
 
-export default XUIButtonCaret;
-
 XUIButtonCaret.propTypes = PropTypes;
-XUIButtonCaret.defaultProps = DefaultProps;
+
+export default XUIButtonCaret;

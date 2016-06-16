2af5d2c85efb7eb15a96a400d33a6f7f45785ff4
Kirk Holloway <kirk.holloway@xero.com>
2016-06-17 09:42:18 +1200

    Add script to modify external links to work within iframes (#1188)
    * Add script to modify external links to work within iframes
    
    * fix to add _blank to github pages that aren't the docs
    
    * Change to target=_parent to avoid check for local xui

--- styleguide/kss-script.js
+++ styleguide/kss-script.js
@@ -6,4 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
 		document.body.classList.toggle('kss-node-is-showing-sidebar');
 		event.preventDefault();
 	});
+
+	//Adding 'target="_blank"' to external refs so they can be accessed from iframes
+	var isExternal = function(url) {
+		var domain = function(url) {
+			return url.replace(/https?:\/\//,'');
+		};
+		var xuiDocsURL = "github.dev.xero.com/pages/UXE/xui/";
+		return (domain(url).substring(0,xuiDocsURL.length) !== xuiDocsURL);
+	}
+	var links = document.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		if (isExternal(links[i].href)){
+			links[i].setAttribute('target', '_parent');
+		}
+	}
 });

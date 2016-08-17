1a83f943fc7d83121087d6667bd686c2da29b929
James Magness <james.magness@xero.com>
2016-08-18 06:59:01 +1200

    Simplify README.md copy. (#1261)
    We use the README.md for the homepage page in XUI docs. This was
    initially done using a Grunt file copy module.
    
    This commit removes the use of grunt:copy to copy the README to
    styleguide.md and directly references the README from kss/config.json.
    
    * Remove call to copy:files
    * Remove copy:files declaration
    * Modify "homepage" prop to reference README.md

--- build/grunt/copy.js
+++ build/grunt/copy.js
@@ -2,14 +2,6 @@
  * Copy configuration for Grunt.
  */
 module.exports = {
-	'files': {
-		expand: true,
-		src: 'README.md',
-		dest: 'kss/styleguide',
-		rename: function(dest, src){
-			return dest + '/' + src.replace('README.md','styleguide.md')
-		}
-	},
 	'xui': {
 		expand: true,
 		src: 'xui.css',

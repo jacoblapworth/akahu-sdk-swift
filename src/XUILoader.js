839d7a89e3e27e3a59c38ef61495bf8b130e22c2
Tim Redmond <tim.redmond@xero.com>
2017-01-19 10:59:33 +1300

    Tidy up, remove CSS classes (#29)
    * Tidy up, remove CSS classes
    
    * Badge XUI 10.19
    
    * Editorconfig

--- src/XUILoader.js
+++ src/XUILoader.js
@@ -1,39 +1,54 @@
 import React from 'react';
 import cn from 'classnames';
-import CSSClasses from 'xui-css-classes';
+
+const baseClass = 'xui-loader';
 
 const sizeMap = {
-	standard: CSSClasses.Loader.BASE,
-	large: CSSClasses.Loader.LARGE,
-	small: CSSClasses.Loader.SMALL
-};
-
-const propTypes = {
-	className: React.PropTypes.string,
-	qaHook: React.PropTypes.string,
-	label: React.PropTypes.string,
-	defaultLayout: React.PropTypes.bool,
-	size: React.PropTypes.oneOf(Object.keys(sizeMap))
-};
-
-const defaultProps = {
-	defaultLayout: true,
-	size: 'standard'
+  standard: '',
+  large: `${baseClass}-large`,
+  small: `${baseClass}-small`
 };
 
 const XUILoader = (props) => {
-	const sizeClass = sizeMap[props.size];
-	const className = cn(CSSClasses.Loader.BASE, sizeClass, props.className, (props.defaultLayout ? CSSClasses.Loader.LAYOUT : ''));
+  const sizeClass = sizeMap[props.size];
+  const className = cn(
+    baseClass,
+    sizeClass,
+    props.className,
+    {
+      [`${baseClass}-layout`] : props.defaultLayout
+    }
+  );
 
-	return (
-		<div data-automationid={props.qaHook} className={className} role="progressbar" aria-label={props.label} aria-busy="true">
-			<div className={CSSClasses.Loader.DOT} />
-			<div className={CSSClasses.Loader.DOT} />
-			<div className={CSSClasses.Loader.DOT} />
-		</div>
-	)
+  return (
+    <div data-automationid={props.qaHook} className={className} role="progressbar" aria-label={props.label} aria-busy="true">
+      {
+        [1,2,3].map(loaderDot => <div className={`${baseClass}--dot`} key={loaderDot}/>)
+      }
+    </div>
+  )
 };
 
-XUILoader.propTypes = propTypes;
-XUILoader.defaultProps = defaultProps;
+XUILoader.propTypes = {
+  /** @property {string} [className] Add additional classes to the loader wrapping div */
+  className: React.PropTypes.string,
+
+  /** @property {string} [qaHook] Adds data-automationid attribute with qaHook contents to the loader wrapping div */
+  qaHook: React.PropTypes.string,
+
+  /** @property {string} [label] adds aria-label to the loader wrapping div */
+  label: React.PropTypes.string,
+
+  /** @property {boolean} [defaultLayout] Defaults to `true`. Sets the default layout class on the loader wrapping div */
+  defaultLayout: React.PropTypes.bool,
+
+  /** @property {string} [size] Sets the size of the loader to be, small, standard (no class added), and large */
+  size: React.PropTypes.oneOf(Object.keys(sizeMap))
+};
+
+XUILoader.defaultProps = {
+  defaultLayout: true,
+  size: 'standard'
+};
+
 export default XUILoader;

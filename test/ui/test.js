358b48c9594be2414e9123f7e6120c473c309d6d
Sarah Lancaster <sarah.lancaster@xero.com>
2017-01-20 14:59:39 +1300

    New example created in test/ui with component renderer

--- test/ui/test.js
+++ test/ui/test.js
@@ -1,111 +1,224 @@
+import 'babel-core/external-helpers.js';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import XUIAvatar, { XUIAvatarGroup } from '../../index.js';
+import XUIAvatar, { XUIAvatarGroup, XUIAvatarCounter, XUISimpleAvatar } from '../../index.js';
+import { sizeClassNames, variantClassNames} from '../../src/constants.js';
 
-ReactDOM.render(
-	<div>
-		<XUIAvatar
-			className="my-custom-class"
-			value="Huey Duck"
-			size="small"
-			identifier="quack"
-		/>
 
-		<br />
+import '../../bower_components/component-renderer/src/renderer.styles.scss';
 
-		<XUIAvatar
-			value="Dewey Duck"
-			size="medium"
-			identifier="decimal"
-		/>
+import {default as RendererUtils} from 'component-renderer';
 
-		<br />
+(function() {
 
-		<XUIAvatar
-			value="Louie Duck"
-			size="large"
-			identifier="quack-a-rooney"
-		/>
+	const avatarKeys = Object.keys(sizeClassNames);
 
-		<br />
+	const avatarVariants = Object.keys(variantClassNames);
 
-		<XUIAvatar
-			value="Fat Louie"
-			size="xlarge"
-			identifier="quack-a-thing"
-		/>
+	const XUISimpleAvatarConfig = {
+		componentName : 'XUISimpleAvatar',
+		devReady : true,
+		properties : [
+			{
+				name: 'className',
+				type: 'text',
+				default: null,
+				description: 'Adds extra classes to the containing element'
+			},
+			{
+				name: 'qaHook',
+				type: 'text',
+				default: null,
+				description: 'Adds data-automationid attribute to the mask and the avatar'
+			},
+			{
+				name: 'variant',
+				type: 'enum',
+				data: avatarVariants,
+				default: avatarVariants[0],
+				description: 'The avatar variant'
+			},
+			{
+				name: 'value',
+				type: 'text',
+				default: 'User Xperience Engineering',
+				description: 'The text to display in the avatar'
+			},
+			{
+				name: 'imageUrl',
+				type: 'text',
+				default: null,
+				description: 'the image the component should render. Initials rendered otherwise'
+			},
+			{
+				name: 'size',
+				type: 'enum',
+				data: avatarKeys,
+				default: avatarKeys[2],
+				description: 'The size of this avatar. small, medium, large, or xlarge'
+			},
+			{
+				name: 'identifier',
+				type: 'text',
+				default: 'purple',
+				description: 'A unique string that will be used to generate the color of the avatar if color is not provided.'
+			},
+			{
+				name: 'onError',
+				type: 'function',
+				default: function(){console.log('close requested')},
+				description: 'Bind a function to fire on error(The test function for this showcase prints to the console)'
+			},
+		]
+	};
 
-		<br />
+	const XUIAvatarCounterConfig = {
+		componentName : 'XUIAvatarCounter',
+		devReady : true,
+		properties : [
+			{
+				name: 'qaHook',
+				type: 'text',
+				default: null,
+				description: 'Adds data-automationid attribute to the mask and the avatar'
+			},
+			{
+				name: 'className',
+				type: 'text',
+				default: null,
+				description: 'Adds extra classes to the containing element'
+			},
+			{
+				name: 'count',
+				type: 'string',
+				default: '4',
+				description: 'The count to display'
+			},
+			{
+				name: 'size',
+				type: 'enum',
+				data: avatarKeys,
+				default: avatarKeys[2],
+				description: 'The size of this avatar. small, medium, large, or xlarge'
+			},
+		]
+	};
 
-		<XUIAvatar
-			value="Madrigal Elektromotoren GmBH"
-			variant="business"
-			size="small"
-			identifier="123"
-		/>
+	const XUIAvatarGroupConfig = {
+		componentName : 'XUIAvatarGroup',
+		devReady : true,
+		properties : [
+			{
+				name: 'className',
+				type: 'text',
+				default: null,
+				description: 'Adds extra classes to the containing element'
+			},
+			{
+				name: 'qaHook',
+				type: 'text',
+				default: null,
+				description: 'Adds data-automationid attribute to the mask and the avatar'
+			},
+			{
+				name: 'children',
+				type: 'text',
+				default: 'Header Children go here',
+				description: 'Add any component you want as children to the footer'
+			},
+			{
+				name: 'size',
+				type: 'enum',
+				data: avatarKeys,
+				default: avatarKeys[2],
+				description: 'The size of this avatar. small, medium, large, or xlarge'
+			},
+			{
+				name: 'maxAvatars',
+				type: 'in',
+				default: '5',
+				description: 'The maximum number of avatars to show'
+			}
+		]
+	};
 
-		<br />
 
-		<XUIAvatar
-			value="A1A Car Wash"
-			variant="business"
-			size="medium"
-			identifier="456"
-		/>
 
-		<br />
+	const XUIAvatarConfig = {
+		componentName : 'XUIAvatar',
+		devReady : true,
+		properties : (() => {
+			return [
+				{
+					name: 'onError',
+					type: 'function',
+					default: function(){console.log('close requested')},
+					description: 'Bind a function to fire on error(The test function for this showcase prints to the console)'
+				},
+				{
+					name: 'value',
+					type: 'string',
+					default: 'The text to display in the avatar',
+					description: 'The text to display in the avatar'
+				},
 
-		<XUIAvatar
-			value="Hornblower Enterprises"
-			variant="business"
-			size="large"
-			identifier="789"
-		/>
+			]
+		})()
+	};
 
-		<br />
+	const ExampleConfig = {
+		componentName : 'Example',
+		devReady : true,
+		properties : [...XUIAvatarConfig.properties]
+	};
 
-		<XUIAvatar
-			value="Vamonos Pest Control"
-			variant="business"
-			size="xlarge"
-			identifier="000"
-		/>
+	class Example extends React.Component {
 
-		<br />
+		constructor(props) {
+			super(props);
 
-		<XUIAvatar
-			value="💩"
-			size="medium"
-		/>
+			this.state = this.props;
+		}
 
-		<br />
+		componentDidUpdate(prevProps) {
+			Object.keys(prevProps).forEach(propName => {
+				if (this.props[propName] !== prevProps[propName]) {
+					this.setState({
+						[propName]: this.props[propName]
+					});
+				}
+			});
+		}
 
-		<XUIAvatar
-			value="Gyro"
-			size="small"
-			imageUrl="logo.png"
-		/>
+		render () {
+			return (
+				<XUIAvatar
+					value="Louie Duck"
+					size="large"
+					identifier="quack-a-rooney"
+				/>
+			)
 
-		<br />
+		}
 
-		<XUIAvatarGroup size="small" maxAvatars={3}>
-			<XUIAvatar value="abcdefg" />
-			<XUIAvatar value="1234" imageUrl="https://example.com/non-existent-url.png" />
-			<XUIAvatar value="asdf" imageUrl="logo.png" />
-		</XUIAvatarGroup>
+	}
 
-		<XUIAvatarGroup maxAvatars={2}>
-			<XUIAvatar value="abcdefg" />
-			<XUIAvatar value="1234" imageUrl="https://example.com/non-existent-url.png" />
-			<XUIAvatar value="asdf" imageUrl="logo.png" />
-		</XUIAvatarGroup>
+	RendererUtils.init({
+		components : {
+			Example,
+			XUIAvatar,
+			XUIAvatarCounter,
+			XUIAvatarGroup,
+			XUISimpleAvatar
+		},
+		configs : {
+			ExampleConfig,
+			XUIAvatarConfig,
+			XUIAvatarCounterConfig,
+			XUIAvatarGroupConfig,
+			XUISimpleAvatarConfig
+		},
+		defaultComponent : Example,
+		defaultConfig: ExampleConfig
+	});
 
-		<XUIAvatarGroup size="large" maxAvatars={1}>
-			<XUIAvatar value="abcdefg" />
-			<XUIAvatar value="1234" imageUrl="https://example.com/non-existent-url.png" />
-			<XUIAvatar value="asdf" imageUrl="logo.png" />
-		</XUIAvatarGroup>
-
-	</div>,
-	document.getElementById('app')
-);
+})();

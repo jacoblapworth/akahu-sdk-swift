cbf5696ae7ddb3e83ef332ddac744480590fd1c8
Sarah Lancaster <sarah.lancaster@xero.com>
2017-01-25 11:21:20 +1300

    Component-renderer used in example

--- test/ui/test.js
+++ test/ui/test.js
@@ -1,92 +1,147 @@
 import 'babel-core/external-helpers.js';
-
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import XUISwitch from '../../src/XUISwitch.js';
+
+import '../../bower_components/component-renderer/src/renderer.styles.scss';
+
+import {default as RendererUtils} from 'component-renderer';
 
 const SwitchModel = (checked, disabled) => ({disabled, checked});
 
-class FakeApp extends Component {
-
-	constructor() {
-		super();
-
-		this.state = {
-			// Sets default disabled for this switch example
-			switch1 : new SwitchModel(true, false),
-			switch2 : new SwitchModel(false, false),
-			switch3 : new SwitchModel(true, true),
-			switch4 : new SwitchModel(false, false)
-		};
-	}
-
-	handleSwitch(thisSwitch, event) {
-
-		const checked = !this.state[thisSwitch].checked;
-
-		this.setState({
-			[thisSwitch] : {
-				checked
-			}
-		}, () => {
-			if (thisSwitch === 'switch4') {
-
-				const newState = {...this.state};
-
-				newState.switch1.disabled = checked;
-				newState.switch2.disabled = checked;
-				newState.switch3.disabled = checked;
-
-				this.setState(newState);
-			}
-		});
-	}
-
-	render() {
-
-		console.log(this.state);
-		const { switch1, switch2, switch3, switch4 } = this.state;
-
-		return (
-
-			<div>
-				<XUISwitch
-					checked={switch1.checked}
-					disabled={switch1.disabled}
-					onChange={this.handleSwitch.bind(this, 'switch1')}
-					value='someValue'
-					name='someName'></XUISwitch>
-				<XUISwitch
-					checked={switch2.checked}
-					disabled={switch2.disabled}
-					onChange={this.handleSwitch.bind(this, 'switch2')}
-					value='someValue'
-					name='someName'></XUISwitch>
-				<XUISwitch
-					checked={switch3.checked}
-					disabled={switch3.disabled}
-					onChange={this.handleSwitch.bind(this, 'switch3')}
-					value='someValue'
-					name='someName'></XUISwitch>
-
-				<h1>Disable all the things</h1>
-
-				<XUISwitch
-					checked={switch4.checked}
-					disabled={switch4.disabled}
-					onChange={this.handleSwitch.bind(this, 'switch4')}
-					value='someValue'
-					name='someName'></XUISwitch>
-			</div>
-		);
-	}
-}
-
 (function() {
 
-	ReactDOM.render(
-		<FakeApp />,
-		document.getElementById('app')
-	);
+	const XUISwitchConfig = {
+		componentName : 'XUISwitch',
+		devReady : true,
+		properties : [
+			{
+				name: 'disabled',
+				type: 'boolean',
+				default: false,
+				description: 'Sets the switch to enabled or disabled'
+			},
+			{
+				name: 'checked',
+				type: 'boolean',
+				default: false,
+				description: 'Sets the switch to be checked or unchecked (on or off)'
+			},
+			{
+				name: 'onChange',
+				type: 'function',
+				default: function(){},
+				description: 'Bind a function to fire when the Switch state changes'
+			},
+			{
+				name: 'value',
+				type: 'text',
+				default: '',
+				description: 'Optional value for the input'
+			},
+			{
+				name: 'name',
+				type: 'text',
+				default: '',
+				description: 'Optional name for the input'
+			}
+		]
+	};
+
+
+
+	const ExampleConfig = {
+		componentName : 'Example',
+		devReady : true,
+		properties : [...XUISwitchConfig.properties]
+	};
+
+	class Example extends React.Component {
+
+		constructor(props) {
+			super(props);
+
+			this.state = {
+				// Sets default disabled for this switch example
+				switch1 : new SwitchModel(true, false),
+				switch2 : new SwitchModel(false, false),
+				switch3 : new SwitchModel(true, true),
+				switch4 : new SwitchModel(false, false)
+			};
+		}
+
+
+		handleSwitch(thisSwitch, event) {
+
+			const checked = !this.state[thisSwitch].checked;
+
+			this.setState({
+				[thisSwitch] : {
+					checked
+				}
+			}, () => {
+				if (thisSwitch === 'switch4') {
+
+					const newState = {...this.state};
+
+					newState.switch1.disabled = checked;
+					newState.switch2.disabled = checked;
+					newState.switch3.disabled = checked;
+
+					this.setState(newState);
+				}
+			});
+		}
+
+		componentDidUpdate(prevProps) {
+			Object.keys(prevProps).forEach(propName => {
+				if (this.props[propName] !== prevProps[propName]) {
+					this.setState({
+						[propName]: this.props[propName]
+					});
+				}
+			});
+		}
+
+		render () {
+			const { switch1, switch2, switch3 } = this.state;
+			return (
+				<div>
+					<XUISwitch
+						checked={switch1.checked}
+						disabled={switch1.disabled}
+						onChange={this.handleSwitch.bind(this, 'switch1')}
+						value='someValue'
+						name='someName'/>
+					<XUISwitch
+						checked={switch2.checked}
+						disabled={switch2.disabled}
+						onChange={this.handleSwitch.bind(this, 'switch2')}
+						value='someValue'
+						name='someName'/>
+					<XUISwitch
+						checked={switch3.checked}
+						disabled={switch3.disabled}
+						onChange={this.handleSwitch.bind(this, 'switch3')}
+						value='someValue'
+						name='someName'/>
+				</div>
+			)
+
+		}
+
+	}
+
+	RendererUtils.init({
+		components : {
+			Example,
+			XUISwitch
+		},
+		configs : {
+			ExampleConfig,
+			XUISwitchConfig
+		},
+		defaultComponent : Example,
+		defaultConfig: ExampleConfig
+	});
 
 })();

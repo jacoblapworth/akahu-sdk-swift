67ec226c6b23071bce5cf5f92ec180f1b923cde0
Sarah.Lancaster <Sarah.Lancaster@xero.com>
2017-01-25 13:40:40 +1300

    Changed import statement

--- test/ui/test.js
+++ test/ui/test.js
@@ -5,7 +5,7 @@ import { sizeClassNames }  from '../../src/private/constants.js';
 
 import '../../bower_components/component-renderer/src/renderer.styles.scss';
 
-import {default as RendererUtils} from 'component-renderer';
+import RendererUtils from 'component-renderer'
 
 (function() {
 

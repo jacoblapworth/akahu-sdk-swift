28a733981a73b6c3d2539ccdc58c06fe649b9c47
Sarah Lancaster <sarah.lancaster@xero.com>
2017-02-01 12:04:09 +1300

    Code review changes made

--- test/ui/test.js
+++ test/ui/test.js
@@ -2,9 +2,7 @@ import 'babel-core/external-helpers.js';
 import React from 'react';
 import XUILoader  from '../../src/XUILoader';
 import { sizeClassNames }  from '../../src/private/constants.js';
-
 import '../../bower_components/component-renderer/src/renderer.styles.scss';
-
 import RendererUtils from 'component-renderer'
 
 (function() {
@@ -18,7 +16,7 @@ import RendererUtils from 'component-renderer'
       {
         name: 'className',
         type: 'string',
-        default: '',
+        default: null,
         description: 'Additional classes to be put on the loader'
       },
       {
@@ -36,22 +34,18 @@ import RendererUtils from 'component-renderer'
       {
         name: 'label',
         type: 'string',
-        default: '',
+        default: null,
         description: 'Adds an aria-label attribute to the loader.'
       },
       {
         name: 'size',
         type: 'enum',
         data: loaderKeys,
-        default: 'loaderKeys[2]',
+        default: loaderKeys[0],
         description: 'Sets the size of the loader to be, small, standard (no class added), and large'
       }
-
-
     ]
   };
-
-
 
   RendererUtils.init({
     components: {

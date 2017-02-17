ac221e4edac25e73129e9d57ce8e3ecc6b7ff712
Ivan Miskovic <ivan.miskovic@xero.com>
2017-02-17 14:15:09 +1300

    Clone the kids

--- src/XUIAvatarGroup.js
+++ src/XUIAvatarGroup.js
@@ -1,8 +1,7 @@
 import React, {PureComponent} from 'react';
 import cn from 'classnames';
+import Guid from 'guid'
 import XUIAvatarCounter from './XUIAvatarCounter';
-import XUIAvatar from './XUIAvatar';
-import XUISimpleAvatar from './XUISimpleAvatar';
 import { sizeClassNames, classNames } from './constants';
 
 const propTypes = {
@@ -13,11 +12,16 @@ const propTypes = {
 	/** @property {String} [avatarSize] The size to apply to all avatars contained within the group. This will override any individual avatar's size settings. */
 	avatarSize: React.PropTypes.oneOf(Object.keys(sizeClassNames)),
 
-	/** @property {Number} [maxAvatars] The maximum number of avatars to show. Must be greater than 0 to take effect */
+	/** @property {Number} [maxAvatars] The maximum number of avatars to show */
 	maxAvatars: function(props, propName) {
 		const maxAvatars = props[propName];
-		if(maxAvatars != null && (typeof maxAvatars !== 'number' || !isFinite(maxAvatars) || maxAvatars <= 0)) {
-			return new Error('maxAvatars prop must be a positive integer greater than zero');
+
+		if(maxAvatars && typeof maxAvatars !== 'number') {
+			return new Error('maxAvatars prop must be a number if specified');
+		}
+
+		if(typeof maxAvatars === 'number' && (!Number.isFinite(maxAvatars) || maxAvatars < 0)) {
+			return new Error('maxAvatars prop must be a positive integer');
 		}
 	}
 };
@@ -26,23 +30,26 @@ export default class XUIAvatarGroup extends PureComponent {
 	render() {
 		const { props } = this;
 		const { maxAvatars, avatarSize } = props;
-		const childCount = (props.children && props.children.length) || 0;
+
+		if(maxAvatars === 0) {
+			return null;
+		}
+
+		const childCount = React.Children.count(props.children);
 		const extraChildCount = (maxAvatars && childCount > maxAvatars) ? childCount - maxAvatars + 1 : 0;
+		const lastChildIndex = extraChildCount ? maxAvatars - 1 : childCount;
 
 		const counter = extraChildCount ? <XUIAvatarCounter count={extraChildCount} size={avatarSize} /> : null;
-		let children = extraChildCount ? props.children.slice(1, maxAvatars) : props.children;
-
 		const className = cn(classNames.group, props.className);
+		let children = props.children;
 
-		if(avatarSize) {
-			children = React.Children.map(children, function(child) {
-				const Type = child.type;
-
-				if(Type === XUIAvatar || Type === XUISimpleAvatar || Type === XUIAvatarCounter) {
-					return <Type {...child.props} size={avatarSize} />;
-				} else {
-					return child;
-				}
+		if(avatarSize || counter) {
+			children = React.Children.map(props.children, function(child, idx) {
+				return idx < lastChildIndex ? React.cloneElement(child, {
+					key: Guid.raw(),
+					...child.props,
+					size: avatarSize
+				}) : null;
 			});
 		}
 

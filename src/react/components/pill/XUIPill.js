235fb652e7ba0ea6b201c6a2c53bcd0cd2db71e1
John Sanders <john.sanders@xero.com>
2017-06-05 18:48:46 +1200

    Import pill

---
+++ src/react/components/pill/XUIPill.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import PropTypes from 'proptypes';
+import cn from 'classnames';
+import crossSmall from '@xero/xui-icon/icons/cross-small';
+
+import XUIIcon from '../icon/XUIIcon';
+import XUIButton from '../button/XUIButton';
+import InnerPill from './InnerPill';
+
+/**
+ * @private
+ * @param {Function} callback
+ * @param {Object} scope
+ * @return {Function} the provided callback with the scope passed in as an argument.
+ */
+function returnCallbackWithScope(callback, scope) {
+	return () => callback(scope);
+}
+
+export default class XUIPill extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isFocussed: false
+		};
+
+		this.toggleFocus = this.toggleFocus.bind(this);
+	}
+
+	toggleFocus() {
+		this.setState(prevState => ({
+			isFocussed: !prevState.isFocussed
+		}));
+	}
+
+	render() {
+		const pill = this;
+
+		const {
+			avatarProps,
+			className,
+			deleteButtonLabel,
+			hasLayout,
+			href,
+			isInvalid,
+			onClick,
+			onDeleteClick,
+			qaHook,
+			secondaryText,
+			target,
+			value
+		} = pill.props;
+
+		const baseClass = 'xui-newpill';
+
+		const pillClasses = cn(
+			className,
+			baseClass,
+			{
+				[`${baseClass}-layout`]: hasLayout,
+				[`${baseClass}-is-invalid`] : isInvalid,
+				[`${baseClass}-is-focussed`]: pill.state.isFocussed,
+				[`${baseClass}-is-deleteable`]: onDeleteClick
+			}
+		);
+
+		const innerPillProps = {
+			avatarProps,
+			href,
+			onClick,
+			qaHook,
+			secondaryText,
+			target,
+			value
+		};
+
+		const onDeleteCallback = onDeleteClick && returnCallbackWithScope(onDeleteClick, pill);
+
+		const deleteButton = onDeleteCallback && (
+			<XUIButton
+				className={`${baseClass}--button-icon`}
+				variant="icon"
+				onClick={onDeleteCallback}
+				title={deleteButtonLabel}
+				aria-label={deleteButtonLabel}
+				qaHook={qaHook ? `deletePillButton-${qaHook}` : null}
+			>
+				<XUIIcon path={crossSmall} />
+			</XUIButton>
+		);
+
+		return (
+			<div
+				className={pillClasses}
+				onFocus={pill.toggleFocus}
+				onBlur={pill.toggleFocus}
+				data-automationid={qaHook}>
+				<InnerPill {...innerPillProps} />
+				{deleteButton}
+			</div>
+		)
+	}
+}
+
+const noop = () => {};
+
+XUIPill.defaultProps = {
+	deleteButtonLabel: 'Delete',
+	hasLayout: true,
+	onClick: noop
+};
+
+XUIPill.propTypes = {
+	/** @property {Object} [avatarProps] Props for the avatar to be displayed, must adhere to the XUIAvatar component API described at https://github.dev.xero.com/UXE/xui-avatar. Version 6.0.0+. Not providing props will omit the avatar entirely. */
+	avatarProps: PropTypes.object,
+	/** @property {String} [className] Apply classes to the outer Pill `div` element. */
+	className: PropTypes.string,
+	/** @property {String} [deleteButtonLabel='Delete'] Specify an alternate label attribute for the delete button, defaults to 'Delete'. */
+	deleteButtonLabel: PropTypes.string,
+	/** @property {Boolean} [hasLayout=true] Remove the XUI layout class by specifiying false. */
+	hasLayout: PropTypes.bool,
+	/** @property {String} [href] This will make the value an `anchor` element instead of a `span` element and adds the href as the link. */
+	href: PropTypes.string,
+	/** @property {String} [isInvalid] When invalid, displays the text in a red colour. */
+	isInvalid: PropTypes.bool,
+	/** @property {Function} [onClick=noop] Callback to fire when the main pill content is clicked. */
+	onClick: PropTypes.func,
+	/** @property {Function} [onDeleteClick] Callback to fire when the delete pill button is clicked. When omitted, the delete button is also ommitted from the view. */
+	onDeleteClick: PropTypes.func,
+	/** @property {String} [qaHook] Adds a data attribute hook for testing. The inner pill has the format `pillButton-${YOUR_STRING_GOES_HERE}`. The delete button has the format `deletePillButton-${YOUR_STRING_GOES_HERE}`. The wrapper pill div has the format `${YOUR_STRING_GOES_HERE}`. */
+	qaHook: PropTypes.string,
+	/** @property {Boolean} [target] When an `href` is supplied, adds a target attribute, else is ignored. */
+	target: PropTypes.string,
+	/** @property {String} [secondaryText] Adds a muted secondary text for the pill, appears before the main value. */
+	secondaryText: PropTypes.string,
+	/** @property {String} [value] The text to display inside the pill. */
+	value: PropTypes.string
+};
---
+++ src/react/components/pill/__tests__/XUIPill-test.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { assert } from 'chai';
+import XUIPill from '../XUIPill.js';
+
+const TestUtils = require('react-dom/test-utils');
+
+describe('<XUIPill />', () => {
+
+	it('renders with correct XUI classes including layout', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill');
+		assert.isTrue(node.classList.contains('xui-newpill-layout'));
+	});
+
+	it('render pill text in a button by default', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill').firstChild;
+		assert.strictEqual(node.tagName, 'BUTTON')
+	});
+
+	it('renders the pill with the specified className prop', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill className='xui-test-class' value="Classy Pill" />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill');
+		assert.isTrue(node.classList.contains('xui-test-class'));
+	});
+
+	it('renders the pill with the specified value prop', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill value="Value Pill" />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill--content');
+		assert.strictEqual(node.textContent, 'Value Pill');
+	});
+
+	it('will render the pill text in an "a" tag when a href prop is provided', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill value="Linky Pill" href="xero.com"/>
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill--content');
+		assert.strictEqual(node.tagName, 'A');
+	});
+
+	it('renders the pill with the invalid class when the isInvalid prop is true', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill isInvalid={true} value="Invalid Pill" />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill');
+		assert.isTrue(node.classList.contains('xui-newpill-is-invalid'));
+	});
+
+	it('will render avatars when passed as an avatar prop', () => {
+		const link = "https://s3.amazonaws.com/uifaces/faces/twitter/kerihenare/24.jpg";
+		const avatarProps = {
+			imageUrl: link,
+			size: 'small',
+			role: 'presentation',
+			value: 'A'
+		};
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill avatarProps={avatarProps} value="Avatar Pill" />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-avatar');
+		assert.strictEqual(node.src, link);
+	});
+
+	it('renders the pill without the xui-newpill--layout class when hasLayout prop is false', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill hasLayout={false} value="Ugly Pill" />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill');
+		assert.isFalse(node.classList.contains('xui-newpill-layout'));
+	});
+
+	it('invokes the callback passed into the onDeleteClick prop with itself passed in as an argument', () => {
+		var value = "Value";
+		const callback = (pill) => {value = pill.props.value};
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill value="Pill" onDeleteClick={callback} />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-button-icon');
+		TestUtils.Simulate.click(node);
+		assert.strictEqual(value, "Pill");
+	});
+
+	it('invokes the callback passed into the onClick prop', () => {
+		const callback = jest.fn();
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill onClick={callback} />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill--content');
+		TestUtils.Simulate.click(node);
+		expect(callback).toHaveBeenCalled();
+	});
+
+	it('renders the text passed in the secondaryText prop', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill secondaryText='supplementary' />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill--content').firstChild;
+		assert.strictEqual(node.textContent, 'supplementary')
+	});
+
+	it('renders itself as focussed appropriately', () => {
+		const pill = TestUtils.renderIntoDocument(
+			<XUIPill />
+		);
+
+		const node = TestUtils.findRenderedDOMComponentWithClass(pill, 'xui-newpill');
+		assert.isFalse(node.classList.contains('xui-newpill-is-focussed'));
+		TestUtils.Simulate.focus(node);
+		assert.isTrue(node.classList.contains('xui-newpill-is-focussed'));
+	});
+});

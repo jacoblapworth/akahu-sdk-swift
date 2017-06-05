750681731a7c6104c02b3355ce3db91882e692a1
John Sanders <john.sanders@xero.com>
2017-06-06 02:19:28 +1200

    Fix toast imports

--- src/react/toast.js
+++ src/react/toast.js
@@ -1,7 +1,7 @@
-import XUIToast from './components/modal/XUIToast';
-import XUIToastAction from './components/modal/XUIToastAction';
-import XUIToastActions from './components/modal/XUIToastActions';
-import XUIToastMessage from './components/modal/XUIToastMessage';
+import XUIToast from './components/toast/XUIToast';
+import XUIToastAction from './components/toast/XUIToastAction';
+import XUIToastActions from './components/toast/XUIToastActions';
+import XUIToastMessage from './components/toast/XUIToastMessage';
 
 export {
 	XUIToast as default,

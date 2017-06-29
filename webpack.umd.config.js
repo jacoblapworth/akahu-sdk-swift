3b0825485a52b08a76facf77f5848573cc19eadc
James <james.magness@xero.com>
2017-06-30 11:14:49 +1200

    Update umd file names
    This CSS file bundles component specific classes so it is different from XUI on the CDN. I've added
    umd into the CSS file name to reflect it goes with the umd bundle.
    
    Brought in the release version No. from package.json and used in file name in preperation for
    publishing to AF or S3 or attaching to the GH release.
    
    ref: none
    
    cc @dev-johnsanders @dev-williamO @UXE/uxe-team

--- webpack.umd.config.js
+++ webpack.umd.config.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
+const version = require('./package.json').version;
 
 module.exports = {
 	entry: path.join(__dirname, 'src', 'react', 'umd.js'),
 	target: 'web',
 	output: {
 		path: path.join(__dirname, 'umd', 'assets'),
-		filename: 'xui.umd.js',
+		filename: `xui.umd.${version}.js`,
 		library: 'XUI',
 		libraryTarget: 'umd'
 	},
 	plugins: [
-		new ExtractTextPlugin('xui.latest.css')
+		new ExtractTextPlugin(`xui.umd.${version}.css`)
 	],
 	externals: {
 			'react': 'React',

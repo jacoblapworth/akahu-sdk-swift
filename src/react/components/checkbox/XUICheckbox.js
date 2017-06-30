e0c9edb66dfd786919e81f0efce5837319c0a1de
John Sanders <john.sanders@xero.com>
2017-06-30 14:50:35 +1200

    Prevent console errors when rendering checkboxes by default

--- src/react/components/checkbox/XUICheckbox.js
+++ src/react/components/checkbox/XUICheckbox.js
@@ -35,8 +35,30 @@ export default class XUICheckbox extends Component {
 		setIndeterminate(this);
 	}
 
+	onClick = () => {
+		setIndeterminate(this);
+	}
+
 	render() {
-		const {tabIndex, children, className, qaHook, iconCheckPath, iconIndeterminatePath, iconMainPath, isChecked, isDisabled, isRequired, isReversed, isLabelHidden, name, onChange, value, svgClassName, labelClassName} = this.props;
+		const {
+			tabIndex,
+			children,
+			className,
+			qaHook,
+			iconCheckPath,
+			iconIndeterminatePath,
+			iconMainPath,
+			isChecked,
+			isDisabled,
+			isRequired,
+			isReversed,
+			isLabelHidden,
+			name,
+			onChange,
+			value,
+			svgClassName,
+			labelClassName,
+		} = this.props;
 		const classes = cn(className, 'xui-styledcheckbox', {
 			'xui-styledcheckbox-reversed': isReversed,
 			'xui-is-disabled': isDisabled
@@ -44,10 +66,34 @@ export default class XUICheckbox extends Component {
 		const svgClasses = cn('xui-icon', svgClassName);
 		const labelClasses = cn('xui-styledcheckbox--label', labelClassName);
 		const labelElement = isLabelHidden ? null : <span className={labelClasses}>{children}</span>;
+		const inputProps = {
+			type: 'checkbox',
+			disabled: isDisabled,
+			required: isRequired,
+			onClick: this.onClick,
+			tabIndex,
+			name,
+			onChange,
+			value,
+		};
+
+		// If the user has not passed in anything for the isChecked prop, we need to set the
+		// `defaultChecked` prop on the input in order to prevent React from outputting warnings
+		// in the console.
+		if (isChecked == null) {
+			inputProps.defaultChecked = this.props.defaultChecked == null ? false : this.props.defaultChecked;
+		} else {
+			inputProps.checked = isChecked;
+			// checked prop without an onChange handler means this is readonly, so set that to prevent
+			// warnings in the console.
+			if (onChange == null) {
+				inputProps.readOnly = true;
+			}
+		}
 
 		return (
 			<label className={classes} data-automationid={qaHook} onClick={onLabelClick}>
-				<input tabIndex={tabIndex} type="checkbox" checked={isChecked} disabled={isDisabled} required={isRequired} name={name} onChange={onChange} value={value} ref={cb => this._input = cb} />
+				<input ref={cb => this._input = cb} {...inputProps} />
 				<svg className={svgClasses}>
 					<path d={iconMainPath || checkboxMain} className="xui-styledcheckbox--focus" role="presentation" />
 					<path d={iconMainPath || checkboxMain} className="xui-styledcheckbox--main" role="presentation" />
@@ -65,52 +111,63 @@ XUICheckbox.propTypes = {
 	className: PropTypes.string,
 	qaHook: PropTypes.string,
 
-	/** @property {string} [iconCheck] - The icon path to use for the checkmark */
+	/** The icon path to use for the checkmark */
 	iconCheckPath: PropTypes.string,
 
-	/** @property {string} [iconIndeterminate] - The icon path to use for the indeterminate mark */
+	/** The icon path to use for the indeterminate mark */
 	iconIndeterminatePath: PropTypes.string,
 
-	/** @property {string} [iconMain] - The icon path to use for the checkbox */
+	/** The icon path to use for the checkbox */
 	iconMainPath: PropTypes.string,
 
-	/** @property {boolean} [isChecked=false] - The input is selected */
+	/** The input is selected */
 	isChecked: PropTypes.bool,
 
-	/** @property {boolean} [isDisabled=false] - The input is disabled */
+	/** The input is disabled */
 	isDisabled: PropTypes.bool,
 
-	/** @property {boolean} [isIndeterminate=false] - The input is indeterminate */
+	/**
+	 * The input is indeterminate.  In order for this prop value to
+	 * stick, you MUST pass in isChecked={false} or a user clicking on this
+	 * will cause React to clear the indeterminate state.
+	 */
 	isIndeterminate: PropTypes.bool,
 
-	/** @property {boolean} [isRequired=false] - The input is required for form submission */
+	/** The input is required for form submission */
 	isRequired: PropTypes.bool,
 
-	/** @property {boolean} [isReversed=false] - The label and control are displayed in reverse order */
+	/** The label and control are displayed in reverse order */
 	isReversed: PropTypes.bool,
 
-	/** @property {string} [labelClassName] - Additional class names on the span (pseudo-label) element  */
+	/** Additional class names on the span (pseudo-label) element  */
 	labelClassName: PropTypes.string,
 
-	/** @property {boolean} [labelHidden=false] - Prevents the label element from being rendered to the page */
+	/** Prevents the label element from being rendered to the page */
 	isLabelHidden: PropTypes.bool,
 
-	/** @property {string} [name] - The name to use as a reference for the value */
+	/** The name to use as a reference for the value */
 	name: PropTypes.string,
 
-	/** @property {function} onChange - The function to call when the control changes state */
+	/** The function to call when the control changes state */
 	onChange: PropTypes.func,
 
-	/** @property {string} [value] - The value to return on form submission */
+	/** The value to return on form submission */
 	value: PropTypes.string,
 
-	/** @property {string} [svgClassName] - Additional class names on the svg element  */
+	/** Additional class names on the svg element  */
 	svgClassName: PropTypes.string,
 
-	/** @property {number} [tabIndex=0] - The tab-index property to place on the checkbox */
-	tabIndex: PropTypes.number
+	/** The tab-index property to place on the checkbox */
+	tabIndex: PropTypes.number,
+
+	/** Used to output an uncontrolled checkbox component */
+	defaultChecked: PropTypes.bool,
 };
 
 XUICheckbox.defaultProps = {
-	isLabelHidden: false
+	isLabelHidden: false,
+	isDisabled: false,
+	isIndeterminate: false,
+	isRequired: false,
+	isReversed: false,
 };
--- src/react/components/checkbox/uitest/demo.js
+++ src/react/components/checkbox/uitest/demo.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import desktop from '@xero/xui-icon/icons/desktop';
 import XUICheckbox from '../XUICheckbox';
 import XUICheckboxGroup from '../XUICheckboxGroup';
-import desktop from '@xero/xui-icon/icons/desktop';
+Object.assign = require('object-assign'); // Make IE11 happy
+
+class Uncontrolled extends React.Component {
+	state = {}
+
+	onChange = event => this.setState({ checked: event.target.checked })
+
+	render() {
+		return (
+			<XUICheckbox checked={this.state.checked} onChange={this.onChange} />
+		);
+	}
+}
 
 const rowClasses = 'xui-row-flex xui-space-around xui-margin-bottom-small';
 const normal = (
@@ -12,7 +25,7 @@ const normal = (
 			<XUICheckbox>Uncontrolled</XUICheckbox>
 			<XUICheckbox isChecked={false}>Unchecked</XUICheckbox>
 			<XUICheckbox isChecked>Checked</XUICheckbox>
-			<XUICheckbox isIndeterminate>Indeterminate</XUICheckbox>
+			<XUICheckbox isIndeterminate isChecked={false}>Indeterminate</XUICheckbox>
 			<XUICheckbox isDisabled>Disabled Unchecked</XUICheckbox>
 			<XUICheckbox isDisabled isChecked>Disabled Checked</XUICheckbox>
 			<XUICheckbox isDisabled isIndeterminate>Disabled Indeterminate</XUICheckbox>
@@ -27,7 +40,7 @@ const reversed = (
 			<XUICheckbox isReversed>Uncontrolled</XUICheckbox>
 			<XUICheckbox isReversed isChecked={false}>Unchecked</XUICheckbox>
 			<XUICheckbox isReversed isChecked>Checked</XUICheckbox>
-			<XUICheckbox isReversed isIndeterminate>Indeterminate</XUICheckbox>
+			<XUICheckbox isReversed isIndeterminate isChecked={false}>Indeterminate</XUICheckbox>
 			<XUICheckbox isReversed isDisabled>Disabled Unchecked</XUICheckbox>
 			<XUICheckbox isReversed isDisabled isChecked>Disabled Checked</XUICheckbox>
 			<XUICheckbox isReversed isDisabled isIndeterminate>Disabled Indeterminate</XUICheckbox>
@@ -48,7 +61,7 @@ const customIcon = (
 			<XUICheckbox iconMainPath={desktop}isChecked>
 				Checked
 			</XUICheckbox>
-			<XUICheckbox iconMainPath={desktop} isIndeterminate>
+			<XUICheckbox iconMainPath={desktop} isIndeterminate isChecked={false}>
 				Indeterminate
 			</XUICheckbox>
 			<XUICheckbox iconMainPath={desktop} isDisabled>
@@ -57,7 +70,7 @@ const customIcon = (
 			<XUICheckbox iconMainPath={desktop} isDisabled isChecked>
 				Disabled Checked
 			</XUICheckbox>
-			<XUICheckbox iconMainPath={desktop} isDisabled isIndeterminate>
+			<XUICheckbox iconMainPath={desktop} isDisabled isIndeterminate isChecked={false}>
 				Disabled Indeterminate
 			</XUICheckbox>
 		</div>
@@ -81,6 +94,9 @@ ReactDOM.render(
 		{reversed}
 		{customIcon}
 		{group}
+		<div>
+			<Uncontrolled />
+		</div>
 	</div>,
 	document.getElementById('app')
 );

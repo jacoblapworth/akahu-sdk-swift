4fe84e4814aa623061bf8104f2f2396442ae6931
Fay Pickering <fay.pickering@xero.com>
2017-08-16 11:25:38 +1200

    remove private components (#2163)

--- src/react/dropdown.js
+++ src/react/dropdown.js
@@ -2,13 +2,11 @@ import DropDown from './components/dropdown/DropDown';
 import DropDownHeader from './components/dropdown/DropDownHeader';
 import DropDownFooter from './components/dropdown/DropDownFooter';
 import DropDownToggled from './components/dropdown/DropDownToggled';
-import DropDownListBox from './components/dropdown/DropDownListBox';
 import DropDownPanel from './components/dropdown/DropDownPanel';
 import NestedDropDown from './components/dropdown/NestedDropDown';
 
 export {
 	DropDown as default,
-	DropDownListBox,
 	DropDownToggled,
 	DropDownHeader,
 	DropDownFooter,

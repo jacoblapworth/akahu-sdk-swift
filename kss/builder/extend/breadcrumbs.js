ad5fa919541e0425bde76aafe772642b1c78bf2c
Isaac Minogue <isaac.minogue@xero.com>
2017-09-05 15:23:30 +1200

    breadcrumb tidy up (#2364)
    * breadcrumb tidy up
    
    * hover
    
    * renamed classes

--- kss/builder/extend/breadcrumbs.js
+++ kss/builder/extend/breadcrumbs.js
@@ -2,10 +2,10 @@ module.exports = function (handlebars) {
 	const breadcrumbPartial = `
 		{{#ifNotInMenu sections.0.header menu}}
 	    <nav>
-	      <ul class="xui-steps xui-steps-interactive xui-u-flex-justify-left">
+	      <ul class="ds-steps xui-steps xui-steps-interactive xui-u-flex-justify-left">
 				<li class="ds-step xui-step xui-step-layout xui-step-is-complete">
-					<a class="xui-step--body ds-step--body" href="index.html">XUI</a>
-					<svg focusable="false" class="xui-step--icon xui-icon xui-u-rotate-270"><use xlink:href="#xui-icon-arrow-small" role="presentation" /></svg>
+					<a class="ds-step--body xui-step--body" href="index.html">XUI</a>
+					<svg focusable="false" class="ds-step--icon xui-step--icon xui-icon xui-u-rotate-270"><use xlink:href="#xui-icon-arrow-small" role="presentation" /></svg>
 				</li>
 				{{#each @root.menu}}
 					{{#if isActive}}
@@ -24,7 +24,7 @@ module.exports = function (handlebars) {
 			<a class="ds-step--body xui-step--body" href="section-{{referenceURI}}.html">{{header}}</a>
 			{{#if menu}}
 				{{#ifNotInMenu pageReference menu}}
-						<svg focusable="false" class="xui-step--icon xui-icon xui-u-rotate-270"><use xlink:href="#xui-icon-arrow-small" role="presentation" /></svg>
+						<svg focusable="false" class="ds-step--icon xui-step--icon xui-icon xui-u-rotate-270"><use xlink:href="#xui-icon-arrow-small" role="presentation" /></svg>
 				{{/ifNotInMenu}}
 			{{/if}}
 		</li>

b37123328c8979a3e11eaeefe9d9bf5ed4519b70
Josh Jacobsen <Josh.Jacobsen@xero.com>
2017-09-19 14:13:56 +1200

    Add autoupdate functionality to XUI Docs so newest version of xui-icon is always used (#2467)

--- kss/builder/extend/version.js
+++ kss/builder/extend/version.js
@@ -1,8 +1,22 @@
 const version = require('../../../package.json').version;
+const iconVersion = require('../../../node_modules/@xero/xui-icon/package.json').version;
+const xuiIcon = `https://edge.xero.com/style/xui-icon/${iconVersion}`
+const xuiIconBlob = `${xuiIcon}/xuiIconBlobES5.js`
+
 module.exports = function(handlebars) {
 
 	handlebars.registerHelper('version', function() {
 		// Returns XUI version as per package.json
 		return new handlebars.SafeString(version);
+	});
+
+	handlebars.registerHelper('xuiIconScripts', function() {
+		// Returns XUI version as per package.json
+		return new handlebars.SafeString(`<script src="${xuiIconBlob}"></script>
+			<script src="${xuiIcon}/xuiIconsDocs.js"></script>`);
+	});
+
+	handlebars.registerHelper('injectXuiIconBlobExample', function() {
+		this.description = this.description.replace('@xuiIconBlobURL', `<code>&lt;script src=&quot;${xuiIconBlob}&quot;&gt;&lt;/script&gt;</code>`);
 	});
 }

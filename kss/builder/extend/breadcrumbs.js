3b986009a8c13b755d755d724cb7369b1dfa7ff5
James Magness <james.magness@xero.com>
2017-09-25 14:57:48 +1300

    Re-factor breadcrumbs (#2516)
    This PR re-factors the breadcrumbs partials to use `isActive` and
    `parentHeader` properties of menu items to figure out which items to
    show.
    
    * isActive is applied to all decendants of the current page
    * parentHeader is available on all child pages
    
    `parentHeader` gives the correct page without needing to test for last
    node or anything like this.
    
    * we were already overwriting `ds-step--icon`. This PR changes it to
    position from left to prevent trailing arrows.
    
    fixes: #2485
    
    cc @UXE/uxe-team

--- kss/builder/extend/breadcrumbs.js
+++ kss/builder/extend/breadcrumbs.js
@@ -1,43 +1,31 @@
 module.exports = function (handlebars) {
-	const breadcrumbPartial = `
-		{{#ifNotInMenu sections.1.header menu}}
-	    <nav>
-	      <ul class="ds-steps xui-steps xui-steps-interactive xui-u-flex-justify-left">
+	handlebars.registerPartial('breadcrumbContent', `
+		{{#if isActive}}
+			{{#if parentHeader}}
+				<li class="ds-step xui-step xui-step-layout xui-step-is-complete">
+					<a class="ds-step--body xui-step--body" href="section-{{referenceURI}}.html">
+						<span class="">{{parentHeader}}</span>
+						<svg focusable="false" class="ds-step--icon xui-step--icon xui-icon xui-u-rotate-270"><use xlink:href="#xui-icon-arrow-small" role="presentation" /></svg>
+					</a>
+				</li>
+			{{/if}}
+			{{#each menu }} {{! Each item is an "li" }}
+				{{> breadcrumbContent}}
+			{{/each}}
+		{{/if}}`
+	);
+
+	handlebars.registerPartial('breadcrumbs', `
+		<nav>
+			<ul class="ds-steps xui-steps xui-steps-interactive xui-u-flex-justify-left">
 				<li class="ds-step xui-step xui-step-layout xui-step-is-complete">
 					<a class="ds-step--body xui-step--body" href="index.html">XUI</a>
-					<svg focusable="false" class="ds-step--icon xui-step--icon xui-icon xui-u-rotate-270"><use xlink:href="#xui-icon-arrow-small" role="presentation" /></svg>
 				</li>
-				{{#each @root.menu}}
-					{{#if isActive}}
-						{{> breadcrumbContent}}
-					{{/if}}
+				{{#each menu}}
+					{{> breadcrumbContent }}
 				{{/each}}
+			</ul>
+		</nav>`
+	);
 
-				</ol>
-			</nav>
-		{{/ifNotInMenu}}
-	`
-
-	const breadcrumbContentsPartial = `
-	{{#isNotEqual header pageReference}}
-		<li class="ds-step xui-step xui-step-layout xui-step-is-complete xui-u-flex-justify-left">
-			<a class="ds-step--body xui-step--body" href="section-{{referenceURI}}.html">{{header}}</a>
-			{{#if menu}}
-				{{#ifNotInMenu pageReference menu}}
-						<svg focusable="false" class="ds-step--icon xui-step--icon xui-icon xui-u-rotate-270"><use xlink:href="#xui-icon-arrow-small" role="presentation" /></svg>
-				{{/ifNotInMenu}}
-			{{/if}}
-		</li>
-	{{/isNotEqual}}
-
-		{{#each menu as |childItem|}}
-			{{#if childItem.isActive}}
-				{{> breadcrumbContent}}
-			{{/if}}
-		{{/each}}
-
-	`
-
-	handlebars.registerPartial('breadcrumbContent', breadcrumbContentsPartial);
-	handlebars.registerPartial('breadcrumbs', breadcrumbPartial);
 }

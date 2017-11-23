18ad07f83509a79b492f6fa024b3222a48b114f0
Tim Redmond <tim.redmond@xero.com>
2017-11-24 11:26:34 +1300

    It's a precommit that does visual things, this time with some NVM love

--- backstop.js
+++ backstop.js
@@ -1,7 +1,15 @@
+const { execSync } = require('child_process');
+
 const XUIPillStories = require('./src/react/components/pill/stories/variations.js').variations;
 
+execSync('npm run storybook:pr', (err, stdout, stderr) => {
+	if (err) { console.error(`Exec error: ${err}`); } //eslint-disable-line no-console
+	console.log(`stdout: ${stdout}`); //eslint-disable-line no-console
+	console.log(`stderr: ${stderr}`); //eslint-disable-line no-console
+});
+
 function buildUrl(kind, story) {
-	const testingDomain = 'http://localhost:9001/iframe.html?';
+	const testingDomain = './.out/iframe.html?';
 	return `${testingDomain}selectedKind=${kind}&selectedStory=${story}`;
 }
 
@@ -14,31 +22,31 @@ const XUIPillScenarios = XUIPillStories.map(story => {
 });
 
 module.exports = {
-  id: "backstop_default",
-  viewports: [
-    {
-      label: "just desktop",
-      width: 1920,
-      height: 1080
-    }
-  ],
-  onBeforeScript: "chromy/onBefore.js",
-  onReadyScript: "chromy/onReady.js",
-  scenarios: [
+	id: "backstop_default",
+	viewports: [
+		{
+			label: "just desktop",
+			width: 1920,
+			height: 1080
+		}
+	],
+	onBeforeScript: "chromy/onBefore.js",
+	onReadyScript: "chromy/onReady.js",
+	scenarios: [
 		...XUIPillScenarios
-  ],
-  paths: {
-    bitmaps_reference: "backstop_data/bitmaps_reference",
-    bitmaps_test: "backstop_data/bitmaps_test",
-    engine_scripts: "backstop_data/engine_scripts",
-    html_report: "backstop_data/html_report",
-    ci_report: "backstop_data/ci_report"
-  },
-  report: ["browser", "CI"],
-  engine: "chrome",
-  engineFlags: [],
-  asyncCaptureLimit: 5,
-  asyncCompareLimit: 50,
-  debug: false,
-  debugWindow: false
+	],
+	paths: {
+		bitmaps_reference: "backstop_data/bitmaps_reference",
+		bitmaps_test: "backstop_data/bitmaps_test",
+		engine_scripts: "backstop_data/engine_scripts",
+		html_report: "backstop_data/html_report",
+		ci_report: "backstop_data/ci_report"
+	},
+	report: ["browser", "CI"],
+	engine: "chrome",
+	engineFlags: [],
+	asyncCaptureLimit: 5,
+	asyncCompareLimit: 50,
+	debug: false,
+	debugWindow: false
 }

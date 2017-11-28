be0f58d4d89ba31282b09de37b4abfdbe2a71856
Tim Redmond <tim.redmond@xero.com>
2017-11-29 07:14:32 +1300

    Adds stories + small bug fix for an npm task

---
+++ src/react/components/autocompleter/stories/variations.js
@@ -0,0 +1,57 @@
+const storiesWithVariationsKindName = 'Instances/XUIAutocompleter';
+
+const NOOP = () => {};
+
+const dropdownSizes = ['small', 'medium', 'large', 'xlarge'];
+
+const variations = [
+	{
+		storyKind: storiesWithVariationsKindName,
+		storyTitle: 'looks like an input',
+		openDrawer: false
+	},
+	// {
+	// 	storyKind: storiesWithVariationsKindName,
+	// 	storyTitle: 'with placeholders',
+	// 	placeholder: 'I am a placeholder'
+	// }, // This test currently does not work
+	{
+		storyKind: storiesWithVariationsKindName,
+		storyTitle: 'shows loading when passed loading prop and dropdown is open',
+		openDrawer: true,
+		isLoading: true
+	},
+	{
+		storyKind: storiesWithVariationsKindName,
+		storyTitle: 'when open shows drawer',
+		openDrawer: true
+	},
+	{
+		storyKind: storiesWithVariationsKindName,
+		storyTitle: 'shows a pill when an item is selected',
+		selectedPeople: 0
+	},
+	...dropdownSizes.map(dropdownSize => ({
+		storyKind: storiesWithVariationsKindName,
+		storyTitle: `shows a ${dropdownSize} dropdown`,
+		openDrawer: true,
+		dropdownSize
+	})),
+	{
+		storyKind: storiesWithVariationsKindName,
+		storyTitle: 'is disabled',
+		isDisabled: true
+	},
+	{
+		storyKind: storiesWithVariationsKindName,
+		storyTitle: 'no drawer footer',
+		openDrawer: true,
+		noDrawerFooter: true
+	},
+];
+
+module.exports = {
+	storiesWithVariationsKindName,
+	variations,
+	NOOP
+}

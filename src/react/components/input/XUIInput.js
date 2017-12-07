b1af0e558a9c75b4892df0275aeefdfc336e111d
Fay Pickering <fay.pickering@xero.com>
2017-12-08 09:14:08 +1300

    [breaking-changes] Re-introduce stateless input (#2815)
    * reverted back to seperate comps + tests
    
    * stateless comp file

--- src/react/components/input/XUIInput.js
+++ src/react/components/input/XUIInput.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import cn from 'classnames';
 import clear from '@xero/xui-icon/icons/clear';
 import XUIIcon from '../icon/XUIIcon';
+import XUIStatelessInput from './XUIStatelessInput';
 import XUIButton from '../button/XUIButton';
 import { compose } from '../helpers/compose';
 
@@ -53,7 +54,6 @@ export default class XUIInput extends Component {
 
 	render() {
 		const statefulInput = this;
-		let iconComponent;
 		const {
 			clearButtonProps,
 			hasClearButton,
@@ -61,15 +61,7 @@ export default class XUIInput extends Component {
 			inputRef,
 			isBorderless,
 			qaHook,
-			iconAttributes,
-			className,
-			containerClassName,
-			isFieldLayout,
-			button,
-			validationMessage,
-			hintMessage,
-			isInvalid,
-			onChange
+			...other
 		} = statefulInput.props;
 
 		const clearButtonIconWrapperClassName = hasClearButton && cn(
@@ -91,101 +83,15 @@ export default class XUIInput extends Component {
 				</XUIButton>
 			</div> : null;
 
-		const hasIcon = !!(iconAttributes && iconAttributes.path);
-
-		const baseClass = 'xui-input';
-		const hasLeftIcon = `${baseClass}-has-left-icon`;
-		const hasRightIcon = `${baseClass}-has-right-icon`;
-
-		const inputWrapperClasses = cn(
-			`${baseClass}wrapper`,
-			containerClassName,
-			{
-				'xui-field-layout': isFieldLayout
-			}
-		);
-
-		let inputClasses = cn(
-			baseClass,
-			className,
-			{
-				[`${baseClass}-is-invalid`] : isInvalid,
-				[`${baseClass}-borderless`] : isBorderless
-			}
-		);
-		
-		// null is not a valid value
-		if(inputAttributes && inputAttributes.defaultValue === null) {
-			inputAttributes.defaultValue = '';
-		}
-
-		if(inputAttributes && inputAttributes.value === null) {
-			inputAttributes.value = '';
-		}
-
-		const message = (validationMessage || hintMessage) && (
-			<div className={cn(
-				'xui-validation',
-				'xui-validation-layout',
-				{ 'xui-validation-is-invalid': isInvalid && validationMessage }
-			)}>{(isInvalid && validationMessage) ? validationMessage : hintMessage}</div>
-		);
-
-		if (hasIcon) {
-			if ((!iconAttributes.position || iconAttributes.position == "left") && !inputClasses.includes(hasLeftIcon)) {
-				inputClasses += ` ${hasLeftIcon}`;
-				if (button && !inputClasses.includes(hasRightIcon)) {
-					inputClasses += ` ${hasRightIcon}`
-				}
-			} else if (!inputClasses.includes(hasRightIcon)) {
-				inputClasses += ` ${hasRightIcon}`;
-			}
-
-			const iconClass = `${baseClass}--icon-${iconAttributes.position || 'left'}`;
-			const iconWrapperClass = hasIcon && `${baseClass}--iconwrapper`;
-
-			const iconWrapperClasses = cn(
-				iconWrapperClass,
-				`${iconWrapperClass}-${iconAttributes.position}`,
-				{
-					[`${iconWrapperClass}-${iconAttributes.wrapperColor}`]: iconAttributes.wrapperColor != null
-				});
-
-			iconComponent = iconWrapperClass ? (
-				<div className={iconWrapperClasses}>
-					<XUIIcon
-						className={iconClass}
-						{...iconAttributes}
-					/>
-				</div>
-			) : (
-				<XUIIcon
-					className={iconClass}
-					{...iconAttributes}
-				/>
-			)
-		} else if (button && !inputClasses.includes(hasRightIcon)) {
-			inputClasses += ` ${hasRightIcon}`;
-		}
-
-
-
 		return (
-			<div
-				data-automationid={qaHook && `${qaHook}--container`}
-				className={inputWrapperClasses}>
-				{iconComponent}
-				<input
-					ref={compose(inputRef, n => statefulInput.inputNode = n)}
-					data-automationid={qaHook}
-					className={inputClasses}
-					onChange={onChange}
-					onKeyUp={compose(inputAttributes && inputAttributes.onKeyUp, () => { onInputChange(statefulInput) })}
-					{...inputAttributes}
-				/>
-				{clearButton}
-				{message}
-			</div>
+			<XUIStatelessInput
+				inputAttributes={{...inputAttributes, onKeyUp: compose(inputAttributes && inputAttributes.onKeyUp, () => { onInputChange(statefulInput) })}}
+				inputRef={compose(inputRef, n => statefulInput.inputNode = n)}
+				button={clearButton}
+				isBorderless={isBorderless}
+				qaHook={qaHook}
+				{...other}
+			/>
 		);
 	}
 }
@@ -215,34 +121,4 @@ XUIInput.propTypes = {
 	isBorderless: PropTypes.bool,
 	/** Function to add a reference to the Input element */
 	inputRef: PropTypes.func,
-	/** Object containing button element placed after input in dom */
-	button: PropTypes.element,
-	/** Class to apply to the input element */
-	className: PropTypes.string,
-	/** Class to apply to the container element */
-	containerClassName: PropTypes.string,
-	/** Object containing icon element related properties */
-	iconAttributes: PropTypes.shape({
-		path: PropTypes.string,
-		position: PropTypes.oneOf(['left', 'right']),
-		wrapperColor: PropTypes.oneOf(['twitter', 'facebook', 'linkedin'])
-	}),
-	/** Validation message to show */
-	validationMessage: PropTypes.node,
-	/** Explanatory message to show */
-	hintMessage: PropTypes.node,
-	/** Whether the input should be show as invalid */
-	isInvalid: PropTypes.bool,
-	/** Whether to use the input field layout */
-	isFieldLayout: PropTypes.bool,
-	onChange: PropTypes.func
-};
-
-XUIInput.defaultProps = {
-	inputAttributes: {
-		type : 'text',
-		disabled: false,
-		readOnly: false
-	},
-	isInvalid: false
 };

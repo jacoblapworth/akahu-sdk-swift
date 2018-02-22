00c03b82f131fe8be4176c5c78176e622fd60d4d
Ivan Miskovic <ivan.miskovic@xero.com>
2018-02-23 11:52:07 +1300

    WIP Isolation mode

---
+++ src/react/components/isolationheader/__tests__/XUIIsolationHeader-test.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+import XUIIsolationHeader from '../XUIIsolationHeader';
+import XUIIsolationHeaderActions from '../XUIIsolationHeaderActions';
+import XUIIsolationHeaderNavigation from '../XUIIsolationHeaderNavigation';
+import XUIIsolationHeaderTitle from '../XUIIsolationHeaderTitle';
+import XUIIsolationHeaderSecondaryTitle from '../XUIIsolationHeaderSecondaryTitle';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<XUIIsolationHeader>', () => {
+	describe('Standard', () => {
+		it('should render an automation id when a qaHook is passed in', () => {
+			const qaHook = "qahook";
+
+			const wrapper = renderer.create(
+				<XUIIsolationHeader isInverted qaHook={qaHook}>
+					<XUIIsolationHeaderNavigation>
+						<XUIIsolationHeaderTitle>
+							My title
+						</XUIIsolationHeaderTitle>
+						<XUIIsolationHeaderSecondaryTitle>
+							My secondary title
+						</XUIIsolationHeaderSecondaryTitle>
+					</XUIIsolationHeaderNavigation>
+					<XUIIsolationHeaderActions>
+						Tools
+					</XUIIsolationHeaderActions>
+				</XUIIsolationHeader>
+			);
+
+			expect(wrapper).toMatchSnapshot();
+		});
+
+		it('should render with the given className if provided', () => {
+			const className = "klass";
+
+			const wrapper = renderer.create(
+				<XUIIsolationHeader isInverted className={className}>
+					<XUIIsolationHeaderNavigation>
+						<XUIIsolationHeaderTitle>
+							My title
+						</XUIIsolationHeaderTitle>
+						<XUIIsolationHeaderSecondaryTitle>
+							My secondary title
+						</XUIIsolationHeaderSecondaryTitle>
+					</XUIIsolationHeaderNavigation>
+					<XUIIsolationHeaderActions>
+						Tools
+					</XUIIsolationHeaderActions>
+				</XUIIsolationHeader>
+			);
+
+			expect(wrapper).toMatchSnapshot();
+		});
+	});
+
+	describe('Inverted', () => {
+		it('should render an automation id when a qaHook is passed in', () => {
+			const qaHook = "qahook";
+
+			const wrapper = renderer.create(
+				<XUIIsolationHeader isInverted qaHook={qaHook}>
+					<XUIIsolationHeaderNavigation>
+						<XUIIsolationHeaderTitle>
+							My title
+						</XUIIsolationHeaderTitle>
+						<XUIIsolationHeaderSecondaryTitle>
+							My secondary title
+						</XUIIsolationHeaderSecondaryTitle>
+					</XUIIsolationHeaderNavigation>
+					<XUIIsolationHeaderActions>
+						Tools
+					</XUIIsolationHeaderActions>
+				</XUIIsolationHeader>
+			);
+
+			expect(wrapper).toMatchSnapshot();
+		});
+
+		it('should render with the given className if provided', () => {
+			const className = "klass";
+
+			const wrapper = renderer.create(
+				<XUIIsolationHeader isInverted className={className}>
+					<XUIIsolationHeaderNavigation>
+						<XUIIsolationHeaderTitle>
+							My title
+						</XUIIsolationHeaderTitle>
+						<XUIIsolationHeaderSecondaryTitle>
+							My secondary title
+						</XUIIsolationHeaderSecondaryTitle>
+					</XUIIsolationHeaderNavigation>
+					<XUIIsolationHeaderActions>
+						Tools
+					</XUIIsolationHeaderActions>
+				</XUIIsolationHeader>
+			);
+
+			expect(wrapper).toMatchSnapshot();
+		});
+	});
+});

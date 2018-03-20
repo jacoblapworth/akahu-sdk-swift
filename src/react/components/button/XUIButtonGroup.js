44b4d13c163922361f28219c1ac7d5260921ff51
dev-ivanm <ivan.miskovic@xero.com>
2018-03-20 16:22:44 +1300

    Button class namespacing

--- src/react/components/button/XUIButtonGroup.js
+++ src/react/components/button/XUIButtonGroup.js
@@ -1,10 +1,11 @@
 import React, { Children, cloneElement } from 'react';
 import PropTypes from "prop-types";
 import cn from 'classnames';
+import {ns} from "../helpers/xuiClassNamespace";
 
 export default function XUIButtonGroup({children, className, qaHook}) {
 	return (
-		<div className={cn(className, 'xui-buttongroup')} data-automationid={qaHook}>
+		<div className={cn(className, `${ns}-buttongroup`)} data-automationid={qaHook}>
 			{Children.map(children, child => cloneElement(child, { isGrouped: true }))}
 		</div>
 	);

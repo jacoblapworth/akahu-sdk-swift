e15e41eefb013195e65ffec9b5edf3b2eb8c929e
Devon Church <devon.church@xero.com>
2018-05-08 10:55:45 +1200

    [minor] Features: Progress Track Weight (#3053)
    * add the thickness prop to the linear progress version
    
    * create segment dots for linear track
    
    * add linear thickness and dots documentation
    
    * add the thickness prop to the circular progress version
    
    * update React documentation
    
    * abstract <ElementSize /> into its own module
    
    * add isGrow back into linear versions with thickness integration
    
    * add React documentation for new functionality
    
    * add stories to accommodate new iterations
    
    * create isolated dot revision
    
    * finesse segment dot variant
    
    * tweak the tangental stroke calculation
    
    * update unit tests to reflect new thickness systems
    
    * update / add visual regression tests
    
    * address ternary / JSX white space issues
    
    * standardise merge anomalies
    
    * re-add visual regression images
    
    * update Jest snapshots
    
    * reenable all visual test sequences

--- src/react/components/progressindicator/__tests__/circularstroke-test.js
+++ src/react/components/progressindicator/__tests__/circularstroke-test.js
@@ -1,40 +1,40 @@
-import {createStrokeWidth} from '../customElements/WithCircularStroke';
+import {createDynamicThickness} from '../customElements/WithCircularGrowth';
 
 describe('<XUIProgressIndicator /> | circular stroke width', () => {
 
-	it('should decrease stroke width as element width inceases', () => {
+	it('should increase stroke width as element width inceases', () => {
 
-		const smallElementStroke = createStrokeWidth(20);
-		const mediumElementStroke = createStrokeWidth(40);
-		const largeElementStroke = createStrokeWidth(60);
+		const smallElementStroke = createDynamicThickness(20);
+		const mediumElementStroke = createDynamicThickness(40);
+		const largeElementStroke = createDynamicThickness(60);
 		const isDecreasing = (
-			smallElementStroke > mediumElementStroke
-			&& mediumElementStroke > largeElementStroke
+			smallElementStroke < mediumElementStroke
+			&& mediumElementStroke < largeElementStroke
 		);
 
 		expect(isDecreasing).toBeTruthy();
 
 	});
 
-	it('should decrease exponentially', () => {
+	it('should increase exponentially', () => {
 
-		const smallElementStroke = createStrokeWidth(20);
-		const mediumElementStroke = createStrokeWidth(40);
-		const largeElementStroke = createStrokeWidth(60);
+		const smallElementStroke = createDynamicThickness(20);
+		const mediumElementStroke = createDynamicThickness(40);
+		const largeElementStroke = createDynamicThickness(60);
 		const isExponential = (
-			(mediumElementStroke - largeElementStroke)
-			> (smallElementStroke - mediumElementStroke)
+			(largeElementStroke - mediumElementStroke)
+			> (mediumElementStroke - smallElementStroke)
 		);
 
 		expect(isExponential).toBeTruthy();
 
 	});
 
-	it('should default to a minimum stroke width of 2 when scaling reaches its threshold', () => {
+	it('should default to a maximum stroke width of 30 when scaling reaches its threshold', () => {
 
-		const extraLargeElementStroke = createStrokeWidth(9999);
+		const extraLargeElementStroke = Math.round(createDynamicThickness(9999));
 
-		expect(extraLargeElementStroke).toBe(2);
+		expect(extraLargeElementStroke).toBe(30);
 
 	});
 

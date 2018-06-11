f16040fba8a3d0697f7d89a0ec1b76940a1902dc
dev-kirkholloway <kirk.holloway@xero.com>
2018-06-11 17:44:37 +1200

    Fix CSS docs for non-boxed icons

--- .kss/builder/kss-assets/copy-code-blocks.js
+++ .kss/builder/kss-assets/copy-code-blocks.js
@@ -1,21 +1,21 @@
 function setInitialButtonStyling(button){
 	button.setAttribute('class', 'clipboard-button xui-button code-copy');
-	button.innerHTML = '<svg focusable="false" class="xui-icon xui-icon-inline"> <use xlink:href="#xui-icon-copy" role="presentation"/></svg>';
+	button.innerHTML = '<svg focusable="false" class="xui-icon xui-blobicon xui-icon-inline"> <use xlink:href="#xui-icon-copy" role="presentation"/></svg>';
 }
 
 function setStandardButtonStyling(button){
 	if (button.classList.contains('clipboard-button')) {
-		button.innerHTML = '<svg focusable="false" class="xui-icon xui-icon-inline"> <use xlink:href="#xui-icon-copy" role="presentation"/></svg>';
+		button.innerHTML = '<svg focusable="false" class="xui-icon xui-blobicon xui-icon-inline"> <use xlink:href="#xui-icon-copy" role="presentation"/></svg>';
 	} else {
-		button.innerHTML = '<svg focusable="false" class="xui-icon"> <use xlink:href="#xui-icon-copy" role="presentation"/></svg>';
+		button.innerHTML = '<svg focusable="false" class="xui-icon xui-blobicon"> <use xlink:href="#xui-icon-copy" role="presentation"/></svg>';
 	}
 }
 
 function setSuccessButtonStyling(button){
 	if (button.classList.contains('clipboard-button')) {
-		button.innerHTML = '<svg focusable="false" class="xui-icon xui-icon-inline"> <use xlink:href="#xui-icon-checkbox-check" role="presentation"/></svg>';
+		button.innerHTML = '<svg focusable="false" class="xui-icon xui-blobicon xui-icon-inline"> <use xlink:href="#xui-icon-checkbox-check" role="presentation"/></svg>';
 	} else {
-		button.innerHTML = '<svg focusable="false" class="xui-icon"> <use xlink:href="#xui-icon-checkbox-check"></use></svg>';
+		button.innerHTML = '<svg focusable="false" class="xui-icon xui-blobicon"> <use xlink:href="#xui-icon-checkbox-check"></use></svg>';
 	}
 }
 

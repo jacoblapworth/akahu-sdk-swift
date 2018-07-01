aeeb62be5acf0e84b8b8f9ec881e38955983e05b
dev-devonchurch <devon.church@xero.com>
2018-07-02 11:30:26 +1200

    Fix negative number discrepancy with empty bar data sets

--- src/react/components/chart/helpers/yaxis.js
+++ src/react/components/chart/helpers/yaxis.js
@@ -33,12 +33,14 @@ export const createYAxisTickValues = ({yAxisHeight, maxValue, minValue}) => {
 		incrementRaw > 1 ? incrementRaw * totalLabels : Math.ceil(maxValue)
 	);
 
-	// When there are no y-axis values the domain is [0, 0, 0] by default. This
-	// blows up Victory so as a fallback (when there is no data and no custom y-axis
-	// value) we simply count up by "1".
-	const hasAnyYValue = Boolean(yAxisMaxValue, minValue);
 
-	const increment = hasAnyYValue ? forceValuePositive(minValue) + yAxisMaxValue / totalLabels : 1;
+	const hasAnyYValue = Boolean(yAxisMaxValue, minValue);
+	const increment = hasAnyYValue
+		? forceValuePositive(minValue) + yAxisMaxValue / totalLabels
+		// When there are no y-axis values the domain is [0, 0, 0] by default. This
+		// blows up Victory so as a fallback (when there is no data and no custom y-axis
+		// value) we simply count up by "1".
+		: -1;
 	const yAxisTickValues = (
 		createArray(totalLabels + 1)
 			.map((_, index) => yAxisMaxValue - (increment * index))

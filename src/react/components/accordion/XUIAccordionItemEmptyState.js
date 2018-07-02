d9f956a8e5be44e839771e33397ec11643cb8f7a
andy.carrell <andy.carrell@xero.com>
2018-07-02 15:45:21 +1200

    Adds accordion item empty state + tests

---
+++ src/react/components/accordion/XUIAccordionItemEmptyState.js
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import XUIIcon from '../icon/XUIIcon';
+import listIconPath from '@xero/xui-icon/icons/list';
+import {ns} from "../helpers/xuiClassNamespace";
+
+export default class XUIAccordionItemEmptyState extends PureComponent {
+	render() {
+		const { children, iconPath, qaHook } = this.props;
+
+		return (
+			<div data-automationid={qaHook} className={`${ns}-textcolor-muted ${ns}-accordionitem-new--emptystate`}>
+				<XUIIcon size="large" path={iconPath} />
+				<div>{children}</div>
+			</div>
+		);
+	}
+}
+
+XUIAccordionItemEmptyState.propTypes = {
+	qaHook: PropTypes.string,
+	children: PropTypes.node,
+	// Path of an icon, consumed by XUIIcon
+	iconPath: PropTypes.string,
+};
+
+XUIAccordionItemEmptyState.defaultProps = {
+	children: 'Nothing available to show',
+	iconPath: listIconPath,
+};

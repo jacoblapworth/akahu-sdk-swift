2b61387dc08656e2562065a86d884092cc7c2232
James Magness <james.magness@xero.com>
2018-08-03 14:09:32 +1200

    update styleguidist to add xui css via config

--- .styleguidist/config.js
+++ .styleguidist/config.js
@@ -92,6 +92,16 @@ const config = {
 		"**/__tests__/**"
 	],
 	sections: componentSections,
+	template: {
+		head: {
+			links: [
+				{
+					rel: 'stylesheet',
+					href: '../dist/xui.css'
+				}
+			]
+		}
+	},
 	getComponentPathLine(componentPath) {
 		let name = path.basename(componentPath, '.js');
 		const dir = path.dirname(componentPath).split('/').pop();

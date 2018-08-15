162d77cb90281985a8e84ed3a649347bc144c5f0
Devon Church <devon.church@xero.com>
2018-08-16 11:52:17 +1200

    charts | update remaining standard colors values to reference 14 swatches

--- src/react/components/chart/helpers/constants.js
+++ src/react/components/chart/helpers/constants.js
@@ -7,7 +7,7 @@ export const CHART_WIDTH = 200;
 export const CHART_FONT_SMALL = 11;
 export const CHART_FONT_LARGE = 13;
 export const CHART_FONT_FAMILY = '"Helvetica Neue", Helvetica, Arial, sans-serif';
-export const CHART_FONT_COLOR = '#657483';
+export const CHART_FONT_COLOR = '#59606D'; // Grey 3
 export const CHART_BREAKPOINT = 520;
 
 export const X_AXIS_HEIGHT = 50;
@@ -20,9 +20,9 @@ export const BAR_MAX_WIDTH = 200;
 export const BAR_RADIUS = 3;
 export const BAR_PADDING_X = 10;
 export const BAR_ACTIVE_COLOR = 'hsla(0, 0%, 100%, 0.25)';
-export const BAR_ODD_COLOR = '#32465A';
-export const BAR_EVEN_COLOR = '#67BBEB';
+export const BAR_ODD_COLOR = '#404756'; // Grey 2
+export const BAR_EVEN_COLOR = '#80BCE4'; // Blue Light
 
-export const GRID_COLOR = '#EBEDEF';
+export const GRID_COLOR = 'rgba(0, 10, 30, 0.1)'; // Grey alpha 7
 
 export const AVATAR_RADIUS = 12;

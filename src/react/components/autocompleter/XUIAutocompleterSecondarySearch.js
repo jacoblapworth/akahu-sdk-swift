d0e994099eee6332e47a4f43b30b96b7976be279
Elise Wei <elise.wei@xero.com>
2018-09-12 10:11:10 +1200

    [breaking-changes] Random bugfixes  (#3770)
    * Updates to fix secondary search
    
    * Fix for compiled CSS breaking lint
    
    * Tweak to stories for inverted styles
    
    * Updated contentblock backstop

--- src/react/components/autocompleter/XUIAutocompleterSecondarySearch.js
+++ src/react/components/autocompleter/XUIAutocompleterSecondarySearch.js
@@ -195,6 +195,7 @@ export default class XUIAutocompleterSecondarySearch extends PureComponent {
 					isBorderlessSolid
 					labelText={inputLabelText}
 					isLabelHidden={isInputLabelHidden}
+					isFieldLayout={false}
 					inputProps={{
 						...inputProps,
 						'id': inputId,

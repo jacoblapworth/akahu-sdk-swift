f8542450389112f18f9a12b5a40be71d44622847
Elise Wei <elise.wei@xero.com>
2018-09-26 10:47:26 +1200

    Codemod for labelText -> label prop renames

--- .codemod/componentTransforms.js
+++ .codemod/componentTransforms.js
@@ -1,5 +1,10 @@
 const stringReplace = require('@xero/xuishift/transforms/stringReplace');
 
+const labelTextToLabel = {
+		name: 'labelText',
+		newName: 'label'
+	};
+
 module.exports = {
 	'@xero/xui/react/pill': [
 		{
@@ -20,7 +25,60 @@ module.exports = {
 					name: 'islabelHidden',
 					newName: 'isLabelHidden'
 				},
+				labelTextToLabel,
 			]
+		}
+	],
+	'@xero/xui/react/autocompleter': [
+		{
+			isDefault: true,
+			props: [
+				{
+					name: 'inputLabelText',
+					newName: 'inputLabel'
+				},
+			]
+		}
+	],
+	'@xero/xui/react/autocompleter': [
+		{
+			name: 'XUIAutocompleterSecondarySearch',
+			props: [
+				{
+					name: 'inputLabelText',
+					newName: 'inputLabel'
+				},
+			]
+		}
+	],
+	'@xero/xui/react/checkbox': [
+		{
+			name: 'XUICheckboxGroup',
+			props: [labelTextToLabel],
+		}
+	],
+	'@xero/xui/react/radio': [
+		{
+			name: 'XUIRadioGroup',
+			props: [labelTextToLabel],
+		}
+	],
+	'@xero/xui/react/rollovercheckbox': [
+		{
+			isDefault: true,
+			props: [labelTextToLabel],
+		}
+	],
+	'@xero/xui/react/textinput': [
+		{
+			isDefault: true,
+			props: [labelTextToLabel],
+		}
+	],
+	'@xero/xui/react/toggle': [
+		{
+			isDefault: true,
+			props: [labelTextToLabel],
 		}
 	],
 }

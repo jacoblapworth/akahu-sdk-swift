c1a00424034243cda0f0ec0bb0ee62f4eeef2a7b
Tim Redmond <tim.redmond@xero.com>
2018-11-27 09:03:05 +1300

    [breaking-changes] Add compositions exports (#3948)

---
+++ src/react/compositions.js
@@ -0,0 +1,23 @@
+import XUICompositionDetail from './components/compositions/XUICompositionDetail';
+import XUICompositionDetailHeader from './components/compositions/XUICompositionDetailHeader';
+import XUICompositionDetailSplit from './components/compositions/XUICompositionDetailSplit';
+import XUICompositionDetailSplitHeader from './components/compositions/XUICompositionDetailSplitHeader';
+import XUICompositionMasterDetail from './components/compositions/XUICompositionMasterDetail';
+import XUICompositionMasterDetailHeader from './components/compositions/XUICompositionMasterDetailHeader';
+import XUICompositionMasterDetailSummary from './components/compositions/XUICompositionMasterDetailSummary';
+import XUICompositionMasterDetailSummaryHeader from './components/compositions/XUICompositionMasterDetailSummaryHeader';
+import XUICompositionSummaryDetail from './components/compositions/XUICompositionSummaryDetail';
+import XUICompositionSummaryDetailHeader from './components/compositions/XUICompositionSummaryDetailHeader';
+
+export {
+	XUICompositionDetail,
+	XUICompositionDetailHeader,
+	XUICompositionDetailSplit,
+	XUICompositionDetailSplitHeader,
+	XUICompositionMasterDetail,
+	XUICompositionMasterDetailHeader,
+	XUICompositionMasterDetailSummary,
+	XUICompositionMasterDetailSummaryHeader,
+	XUICompositionSummaryDetail,
+	XUICompositionSummaryDetailHeader,
+};

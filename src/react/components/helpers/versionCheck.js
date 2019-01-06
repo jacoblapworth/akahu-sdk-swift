3f303f5705edc348754b1be26edcc60543393faa
Ivan Miskovic <ivan.miskovic@xero.com>
2019-01-07 12:39:33 +1300

    Add tests

--- src/react/components/helpers/versionCheck.js
+++ src/react/components/helpers/versionCheck.js
@@ -2,6 +2,34 @@
 import React from 'react';
 import versions from './versions';
 
+// This is only exported so it can be tested
+// eslint-disable-next-line import/prefer-default-export
+export function checkReactVersion(actualVersion, requiredVersion, logError) {
+	const reactVersionSegments = requiredVersion.split('.');
+	if (typeof React.version === 'string' && reactVersionSegments[0].indexOf('^') === 0) {
+		const supportedReactMajorVersion = parseInt(reactVersionSegments[0].substring(1));
+		const minimumSupportedReactMinorVersion = parseInt(reactVersionSegments[1]);
+		const minimumSupportedReactPatchVersion = parseInt(reactVersionSegments[2]);
+
+		const actualReactVersionSegments = actualVersion.split('.');
+		const actualReactMajorVersion = parseInt(actualReactVersionSegments[0]);
+		const actualReactMinorVersion = parseInt(actualReactVersionSegments[1]);
+		const actualReactPatchVersion = parseInt(actualReactVersionSegments[2]);
+
+		if (
+			actualReactMajorVersion !== supportedReactMajorVersion
+			|| actualReactMinorVersion < minimumSupportedReactMinorVersion
+			|| (
+				actualReactMinorVersion === minimumSupportedReactMinorVersion
+				&& actualReactPatchVersion < minimumSupportedReactPatchVersion
+			)
+		) {
+			// eslint-disable-next-line max-len
+			logError(`XUI error: The version of React used (${actualVersion}) is incompatible with this version of XUI which requires ${requiredVersion}`);
+		}
+	}
+}
+
 if (process.env.NODE_ENV === 'development') {
 	// eslint-disable-next-line no-console
 	const logWarning = message => console.warn(message);
@@ -44,30 +72,7 @@ if (process.env.NODE_ENV === 'development') {
 			foundXUI = true;
 		});
 
-		const reactVersionSegments = versions.react.split('.');
-
-		if (typeof React.version === 'string' && reactVersionSegments[0].indexOf('^') === 0) {
-			const supportedReactMajorVersion = parseInt(reactVersionSegments[0].substring(1));
-			const minimumSupportedReactMinorVersion = parseInt(reactVersionSegments[1]);
-			const minimumSupportedReactPatchVersion = parseInt(reactVersionSegments[2]);
-
-			const actualReactVersionSegments = React.version.split('.');
-			const actualReactMajorVersion = parseInt(actualReactVersionSegments[0]);
-			const actualReactMinorVersion = parseInt(actualReactVersionSegments[1]);
-			const actualReactPatchVersion = parseInt(actualReactVersionSegments[2]);
-
-			if (
-				actualReactMajorVersion !== supportedReactMajorVersion
-				|| actualReactMinorVersion < minimumSupportedReactMinorVersion
-				|| (
-					actualReactMinorVersion === minimumSupportedReactMinorVersion
-					&& actualReactPatchVersion < minimumSupportedReactPatchVersion
-				)
-			) {
-				// eslint-disable-next-line max-len
-				logError(`XUI error: The version of React used (${React.version}) is incompatible with this version of XUI which requires ${versions.react}`);
-			}
-		}
+		checkReactVersion(React.version, versions.react, logError);
 	};
 
 	if (typeof document !== 'undefined') {

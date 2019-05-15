082c9add3d3325858f6fce791ba834d6e13f052c
Alex Lapwood <alex.lapwood@xero.com>
2019-05-16 09:36:10 +1200

    [patch] [XUI-589] Update content string props to accept nodes (#4237)
    * [XUI-589] Update content string props to accept nodes
    
    * Prettier

--- src/react/components/pill/XUIInnerPill.js
+++ src/react/components/pill/XUIInnerPill.js
@@ -7,103 +7,102 @@ import { ns } from '../helpers/xuiClassNamespace';
 import { baseClass, childSizeClassMap } from './private/constants';
 
 class XUIInnerPill extends PureComponent {
-	render() {
-		const {
-			avatarProps,
-			href,
-			isInvalid,
-			onClick,
-			qaHook,
-			secondaryText,
-			target,
-			title,
-			value,
-			size,
-			innerPillRef,
-			avatar,
-		} = this.props;
+  render() {
+    const {
+      avatarProps,
+      href,
+      isInvalid,
+      onClick,
+      qaHook,
+      secondaryText,
+      target,
+      title,
+      value,
+      size,
+      innerPillRef,
+      avatar,
+    } = this.props;
 
-		const isInteractive = href || onClick;
+    const isInteractive = href || onClick;
 
-		const className = cn(
-			`${baseClass}--content`,
-			isInteractive && `${baseClass}--button`,
-		);
-		const innerPillQaHook = qaHook && `${qaHook}--inner`;
-		const secondaryTextEl = secondaryText && (
-			<span className={`${ns}-color-grey-muted ${baseClass}--secondary`} ref={innerPillRef}>
-				{secondaryText}
-			</span>
-		);
-		const valueEl = value && (
-			<span className={`${baseClass}--text`} ref={!secondaryText && innerPillRef}>{value}</span>
-		);
+    const className = cn(`${baseClass}--content`, isInteractive && `${baseClass}--button`);
+    const innerPillQaHook = qaHook && `${qaHook}--inner`;
+    const secondaryTextEl = secondaryText && (
+      <span className={`${ns}-color-grey-muted ${baseClass}--secondary`} ref={innerPillRef}>
+        {secondaryText}
+      </span>
+    );
+    const valueEl = value && (
+      <span className={`${baseClass}--text`} ref={!secondaryText && innerPillRef}>
+        {value}
+      </span>
+    );
 
-		const contents = (
-			<Fragment>
-				<LeftVisualEl
-					isInvalid={isInvalid}
-					avatarProps={avatarProps}
-					size={childSizeClassMap[size]}
-					avatar={avatar}
-				/>
-				{secondaryTextEl}
-				{valueEl}
-			</Fragment>
-		);
+    const contents = (
+      <Fragment>
+        <LeftVisualEl
+          isInvalid={isInvalid}
+          avatarProps={avatarProps}
+          size={childSizeClassMap[size]}
+          avatar={avatar}
+        />
+        {secondaryTextEl}
+        {valueEl}
+      </Fragment>
+    );
 
-		return href || onClick ? (
-			<XUIButton
-				{...{
-					href,
-					target,
-					title,
-					onClick,
-				}}
-				className={className}
-				isLink={!!href}
-				variant="unstyled"
-				qaHook={innerPillQaHook}
-				size={size}
-			>
-				{contents}
-			</XUIButton>
-		) : (
-			<span className={className} data-automationid={innerPillQaHook}>
-				{contents}
-			</span>
-		);
-	}
+    return href || onClick ? (
+      <XUIButton
+        {...{
+          href,
+          target,
+          title,
+          onClick,
+        }}
+        className={className}
+        isLink={!!href}
+        variant="unstyled"
+        qaHook={innerPillQaHook}
+        size={size}
+      >
+        {contents}
+      </XUIButton>
+    ) : (
+      <span className={className} data-automationid={innerPillQaHook}>
+        {contents}
+      </span>
+    );
+  }
 }
 
 XUIInnerPill.propTypes = {
-	/** Props for the avatar to be displayed, must adhere to the XUIAvatar component API
-	 * described at https://github.dev.xero.com/UXE/xui-avatar. Version 6.0.0+. Not providing
-	 * props will omit the avatar entirely. */
-	avatarProps: PropTypes.object,
-	/** An avatar component. May be used instead of avatarProps */
-	avatar: PropTypes.element,
-	/** This will make the value an `anchor` element instead of a `span` element and adds the
-	 * href as the link. */
-	href: PropTypes.string,
-	/** Callback to fire when the main pill content is clicked. */
-	onClick: PropTypes.func,
-	/** `. */
-	qaHook: PropTypes.string,
-	/** Adds a muted secondary text for the pill, appears before the main value. */
-	secondaryText: PropTypes.string,
-	/** When an `href` is supplied, adds a target attribute, else is ignored. */
-	target: PropTypes.string,
-	/** The title attribute to apply on the pill. */
-	title: PropTypes.string,
-	/** The text to display inside the pill. */
-	value: PropTypes.string,
-	/** The pill is invalid and should display the invalid icon */
-	isInvalid: PropTypes.bool,
-	/** The ref to the text nodes - Used to determine showing tooltips when text is truncated */
-	innerPillRef: PropTypes.object,
-	/** The size of the pill */
-	size: PropTypes.oneOf(Object.keys(childSizeClassMap)),
+  /** Props for the avatar to be displayed, must adhere to the XUIAvatar component API
+   * described at https://github.dev.xero.com/UXE/xui-avatar. Version 6.0.0+. Not providing
+   * props will omit the avatar entirely. */
+  avatarProps: PropTypes.object,
+  /** An avatar component. May be used instead of avatarProps */
+  avatar: PropTypes.element,
+  /** This will make the value an `anchor` element instead of a `span` element and adds the
+   * href as the link. */
+  href: PropTypes.string,
+  /** Callback to fire when the main pill content is clicked. */
+  onClick: PropTypes.func,
+  /** `. */
+  qaHook: PropTypes.string,
+  /** Adds a muted secondary text for the pill, appears before the main value. */
+  secondaryText: PropTypes.node,
+  /** When an `href` is supplied, adds a target attribute, else is ignored. */
+  target: PropTypes.string,
+  /** The title attribute to apply on the pill. */
+  title: PropTypes.string,
+  /** The text to display inside the pill. */
+  value: PropTypes.node,
+  /** The pill is invalid and should display the invalid icon */
+  isInvalid: PropTypes.bool,
+  /** The ref to the text nodes - Used to determine showing tooltips when text is truncated */
+  innerPillRef: PropTypes.object,
+  /** The size of the pill */
+  size: PropTypes.oneOf(Object.keys(childSizeClassMap)),
 };
 
 export default XUIInnerPill;

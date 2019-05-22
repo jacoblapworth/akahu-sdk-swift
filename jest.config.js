ae3e52fb071d1116a4e24899c90c70f6daa15e56
Nick Piesco <nick.piesco@xero.com>
2019-05-22 14:47:50 +1200

    [XUI-261] Prettier changes

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,28 @@
 const config = {
-	bail: false,
-	verbose: true,
-	globals: {
-		window: true
-	},
-	roots: ['<rootDir>/src/react/components'],
-	testRegex: '-test\\.(js|jsx)$',
-	testResultsProcessor: 'jest-teamcity-reporter',
-	testEnvironment: 'jsdom',
-	moduleDirectories: ['node_modules'],
-	collectCoverageFrom: [
-		'**/src/react/components/**/*.{js,jsx}',
-		'!**/src/react/components/**/__tests__/*',
-		'!**/src/react/components/**/stories/*',
-		'!**/node_modules/**',
-		'!**/src/react/components/autocompleter/private/people.js'
-	],
-	moduleNameMapper: {
-		'\\.(scss)$': '<rootDir>/src/__mocks__/styleMock.js',
-		'\\.(png)$': '<rootDir>/src/__mocks__/fileMock.js'
-	},
-	transform: {
-		'^.+\\.jsx?$': 'babel-jest'
-	},
-	setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
-}
+  bail: false,
+  verbose: true,
+  globals: {
+    window: true,
+  },
+  roots: ['<rootDir>/src/react/components'],
+  testRegex: '-test\\.(js|jsx)$',
+  testResultsProcessor: 'jest-teamcity-reporter',
+  testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules'],
+  collectCoverageFrom: [
+    '**/src/react/components/**/*.{js,jsx}',
+    '!**/src/react/components/**/__tests__/*',
+    '!**/src/react/components/**/stories/*',
+    '!**/node_modules/**',
+    '!**/src/react/components/autocompleter/private/people.js',
+  ],
+  moduleNameMapper: {
+    '\\.(scss)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(png)$': '<rootDir>/src/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
 module.exports = config;

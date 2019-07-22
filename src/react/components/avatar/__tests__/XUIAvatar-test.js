99652bb59b403536e0a932976bef1afb4a96507e
Sophia Ritchie <sophia.ritchie@xero.com>
2019-07-22 16:52:23 +1200

    Tests and snapshots update

--- src/react/components/avatar/__tests__/XUIAvatar-test.js
+++ src/react/components/avatar/__tests__/XUIAvatar-test.js
@@ -9,130 +9,136 @@ import XUIAvatar from '../XUIAvatar';
 import { sizeClassNames, variantClassNames } from '../constants';
 
 describe('XUIAvatar', () => {
-  it('should render an `abbr` avatar when I give it an identifier and a value', () => {
-    const test = <XUIAvatar value="Test" identifier="12345-1234-1234-123456" />;
 
-    const snap = renderer.create(test);
-    expect(snap).toMatchSnapshot();
+	it('should render an `abbr` avatar when I give it an identifier and a value', () => {
 
-    const jestDom = shallow(test);
-    expect(jestDom.text()).toBe('T');
-    expect(jestDom.find('abbr')).toHaveLength(1);
-  });
+		const test = <XUIAvatar value="Test" identifier="12345-1234-1234-123456"/>;
 
-  it('should render an `abbr` avatar when I give it a value', () => {
-    const test = <XUIAvatar value="Test" />;
+		const snap = renderer.create(test);
+		expect(snap).toMatchSnapshot();
 
-    const snap = renderer.create(test);
-    expect(snap).toMatchSnapshot();
+		const jestDom = shallow(test);
+		expect(jestDom.text()).toBe('T');
+		expect(jestDom.find('abbr')).toHaveLength(1);
+	});
 
-    const jestDom = shallow(test);
-    expect(jestDom.text()).toBe('T');
-    expect(jestDom.find('abbr')).toHaveLength(1);
-  });
+	it('should render an `abbr` avatar when I give it a value', () => {
 
-  it('should render an `img` avatar when I give it an image', () => {
-    const test = <XUIAvatar imageUrl="https://xui.xero.com/static/xpert-avatar.png" />;
+		const test = <XUIAvatar value="Test"/>;
 
-    const snap = renderer.create(test);
-    expect(snap).toMatchSnapshot();
+		const snap = renderer.create(test);
+		expect(snap).toMatchSnapshot();
 
-    const jestDom = shallow(test);
-    expect(jestDom.find('img')).toHaveLength(1);
-  });
+		const jestDom = shallow(test);
+		expect(jestDom.text()).toBe('T');
+		expect(jestDom.find('abbr')).toHaveLength(1);
+	});
 
-  it('should render a broken image as an `abbr` avatar', () => {
-    const performTestsAfterError = () => {
-      expect(jestDom.text()).toBe('T');
-      expect(jestDom.find('abbr')).toHaveLength(1);
-    };
+	it('should render an `img` avatar when I give it an image', () => {
 
-    const jestDom = mount(
-      <XUIAvatar
-        value="Test"
-        onError={performTestsAfterError}
-        imageUrl="https://xui.xero.com/static/broken-path.jpg"
-      />,
-    );
-  });
+		const test = <XUIAvatar value="Test" imageUrl="https://xui.xero.com/static/xpert-avatar.png"/>;
 
-  it('should render in a range of sizes', () => {
-    Object.keys(sizeClassNames).forEach(size => {
-      const test = <XUIAvatar value="Test" size={size} />;
+		const snap = renderer.create(test);
+		expect(snap).toMatchSnapshot();
 
-      const snap = renderer.create(test);
-      expect(snap).toMatchSnapshot();
-    });
-  });
+		const jestDom = shallow(test);
+		expect(jestDom.find('img')).toHaveLength(1);
+	});
 
-  it('should render in a range of variants', () => {
-    Object.keys(variantClassNames).forEach(variant => {
-      const test = <XUIAvatar value="Test" variant={variant} />;
+	it('should render a broken image as an `abbr` avatar', () => {
 
-      const snap = renderer.create(test);
-      expect(snap).toMatchSnapshot();
-    });
-  });
+		const performTestsAfterError = () => {
+			expect(jestDom.text()).toBe('T');
+			expect(jestDom.find('abbr')).toHaveLength(1);
+		}
 
-  describe('Handlers', () => {
-    it("should throw an error when the image doesn't exist", () => {
-      const onErrorHandler = jest.fn();
-      const mountedComponent = mount(
-        <XUIAvatar
-          value="Test"
-          onError={onErrorHandler}
-          imageUrl="https://xui.xero.com/static/broken-path.jpg"
-        />,
-      );
+		const jestDom = mount(
+			<XUIAvatar
+				value="Test"
+				onError={performTestsAfterError}
+				imageUrl="https://xui.xero.com/static/broken-path.jpg"/>
+		);
 
-      mountedComponent.find('img').simulate('error');
-      expect(onErrorHandler).toHaveBeenCalledTimes(1);
-    });
+	})
 
-    it("should call the onError internal method when the image doesn't exist", () => {
-      const testString = 'test';
-      const onErrorHandler = jest.fn();
+	it('should render in a range of sizes', () => {
+		Object.keys(sizeClassNames).forEach(size => {
+			const test = <XUIAvatar value="Test" size={size} />;
 
-      const mountedComponent = mount(
-        <XUIAvatar
-          value="Test"
-          onError={onErrorHandler}
-          imageUrl="https://xui.xero.com/static/broken-path.jpg"
-        />,
-      );
+			const snap = renderer.create(test);
+			expect(snap).toMatchSnapshot();
+		});
+	});
 
-      mountedComponent.instance().onError(testString);
-      expect(onErrorHandler).toHaveBeenCalledWith(testString);
-    });
-  });
+	it('should render in a range of variants', () => {
+		Object.keys(variantClassNames).forEach(variant => {
+			const test = <XUIAvatar value="Test" variant={variant} />;
 
-  describe('Generic tests', () => {
-    it('should accept a custom className', () => {
-      const testClass = 'test-class';
-      const test = <XUIAvatar value="Test" className={testClass} />;
+			const snap = renderer.create(test);
+			expect(snap).toMatchSnapshot();
+		});
+	});
 
-      const snap = renderer.create(test);
-      expect(snap).toMatchSnapshot();
+	describe('Handlers', () => {
+		it('should throw an error when the image doesn\'t exist', () => {
+			const onErrorHandler = jest.fn();
+			const mountedComponent = mount(
+				<XUIAvatar
+					value="Test"
+					onError={onErrorHandler}
+					imageUrl="https://xui.xero.com/static/broken-path.jpg"/>
+			);
 
-      const jestDom = mount(test);
-      expect(jestDom.getDOMNode().classList.contains(testClass)).toBeTruthy();
-    });
+			mountedComponent.find('img').simulate('error');
+			expect(onErrorHandler).toHaveBeenCalledTimes(1);
 
-    it('should accept a custom qaHook', () => {
-      const testHook = 'test-hook';
-      const test = <XUIAvatar value="Test" qaHook={testHook} />;
+		});
 
-      const snap = renderer.create(test);
-      expect(snap).toMatchSnapshot();
+		it('should call the onError internal method when the image doesn\'t exist', () => {
+			const testString = 'test';
+			const onErrorHandler = jest.fn();
 
-      const jestDom = mount(test);
-      expect(jestDom.getDOMNode().getAttribute('data-automationid')).toBe(testHook);
-    });
-  });
+			const mountedComponent = mount(
+				<XUIAvatar
+					value="Test"
+					onError={onErrorHandler}
+					imageUrl="https://xui.xero.com/static/broken-path.jpg"/>
+			);
 
-  describe('Expected proptype failures', () => {
-    it('should throw an error if you dont pass either a value or an imageUrl property', () => {
-      expect(() => renderer.create(<XUIAvatar />)).toThrow();
-    });
-  });
+			mountedComponent.instance().onError(testString);
+			expect(onErrorHandler).toHaveBeenCalledWith(testString);
+		});
+	});
+
+	describe('Generic tests', () => {
+		it('should accept a custom className', () => {
+			const testClass = 'test-class';
+			const test = <XUIAvatar value="Test" className={testClass}/>;
+
+			const snap = renderer.create(test);
+			expect(snap).toMatchSnapshot();
+
+			const jestDom = mount(test);
+			expect(jestDom.getDOMNode().classList.contains(testClass)).toBeTruthy();
+		});
+
+		it('should accept a custom qaHook', () => {
+			const testHook = 'test-hook';
+			const test = <XUIAvatar value="Test" qaHook={testHook}/>;
+
+			const snap = renderer.create(test);
+			expect(snap).toMatchSnapshot();
+
+			const jestDom = mount(test);
+			expect(jestDom.getDOMNode().getAttribute('data-automationid')).toBe(testHook);
+		});
+	});
+
+	describe('Expected proptype failures', () => {
+		it('should throw an error if you dont pass either a value or an imageUrl property', () => {
+			expect(() => renderer.create(
+				<XUIAvatar imageUrl="https://xui.xero.com/static/broken-path.jpg" />
+			)).toThrow();
+		});
+	});
 });

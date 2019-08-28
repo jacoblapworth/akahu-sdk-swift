b16958153977a205302cd744f399d4f710d1fbd5
Grzegorz Pawłowski-Chudziński <grzegorz.pc@xero.com>
2019-08-28 14:45:14 +1200

    [patch] Copy XUI CSS after storybook build task (#4363)
    * XUI-466 Copy XUI CSS after storybook tasks to mitigate KSS build cleanup.
    
    * XUI-466 Prettier.
    
    * XUI-466 Latest package lock.
    
    * Revert "XUI-466 Latest package lock."
    
    This reverts commit 51a871c787603a3679e17b19cc54d6d0b28b6b7c.

--- scripts/build/index.js
+++ scripts/build/index.js
@@ -2,84 +2,52 @@
 const path = require('path');
 const { taskRunner, rootDirectory, taskRunnerReturns } = require('../helpers');
 const { succeed, fail } = taskRunnerReturns;
-const sassKss = require(path.resolve(
-	rootDirectory,
-	'scripts',
-	'build',
-	'sass',
-	'kss.js'
-));
-const xuiCss = require(path.resolve(
-	rootDirectory,
-	'scripts',
-	'build',
-	'xui.js'
-));
-const buildStyleguide = require(path.resolve(
-	rootDirectory,
-	'scripts',
-	'build',
-	'styleguidist.js'
-));
-const buildStorybook = require(path.resolve(
-	rootDirectory,
-	'scripts',
-	'build',
-	'storybook.js'
-));
-const buildKss = require(path.resolve(
-	rootDirectory,
-	'scripts',
-	'build',
-	'kss.js'
-));
+const sassKss = require(path.resolve(rootDirectory, 'scripts', 'build', 'sass', 'kss.js'));
+const xuiCss = require(path.resolve(rootDirectory, 'scripts', 'build', 'xui.js'));
+const buildStyleguide = require(path.resolve(rootDirectory, 'scripts', 'build', 'styleguidist.js'));
+const buildStorybook = require(path.resolve(rootDirectory, 'scripts', 'build', 'storybook.js'));
+const buildKss = require(path.resolve(rootDirectory, 'scripts', 'build', 'kss.js'));
 const buildTokens = require(path.resolve(
-	rootDirectory,
-	'scripts',
-	'build',
-	'postcss',
-	'tokens.js'
+  rootDirectory,
+  'scripts',
+  'build',
+  'postcss',
+  'tokens.js',
 ));
-const buildUmd = require(path.resolve(
-	rootDirectory,
-	'scripts',
-	'build',
-	'umd_webpack.js'
-));
+const buildUmd = require(path.resolve(rootDirectory, 'scripts', 'build', 'umd_webpack.js'));
 const buildServiceWorker = require(path.resolve(
-	rootDirectory,
-	'scripts',
-	'build',
-	'serviceworker.js'
+  rootDirectory,
+  'scripts',
+  'build',
+  'serviceworker.js',
 ));
 
 async function build() {
-	await taskRunner(taskSpinner => {
-		return Promise.all([sassKss(), xuiCss()])
-			.then(() => {
-				taskSpinner.info('Done with basic build promises');
-				return Promise.all([
-					buildStyleguide(),
-					buildKss({
-						skipPostCss: true
-					}),
-					buildStorybook({
-						skipPostCss: true
-					}),
-					buildTokens(),
-					buildUmd()
-				]).then(({ stdout, stderr }) => console.log(stdout, stderr));
-			})
-			.then(buildServiceWorker())
-			.then(succeed)
-			.catch(fail);
-	}, __filename);
+  await taskRunner(taskSpinner => {
+    return Promise.all([sassKss(), xuiCss()])
+      .then(() => {
+        taskSpinner.info('Done with basic build promises');
+        return Promise.all([
+          buildStyleguide(),
+          buildKss({
+            skipPostCss: true,
+          }),
+          buildStorybook({
+            skipPostCss: false, // KSS build (build/kss/index.js) cleans the whole storybook directory with copied xui.css and xui-base.css. Keeping post css task restores it.
+          }),
+          buildTokens(),
+          buildUmd(),
+        ]).then(({ stdout, stderr }) => console.log('stdout and stderr >>> ', stdout, stderr));
+      })
+      .then(buildServiceWorker())
+      .then(succeed)
+      .catch(fail);
+  }, __filename);
 
-	process.exit(0);
-
+  process.exit(0);
 }
 
 module.exports = build;
 require('make-runnable/custom')({
-	printOutputFrame: false
+  printOutputFrame: false,
 });

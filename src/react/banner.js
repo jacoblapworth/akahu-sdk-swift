146215d2c3198729308703b3dd7e667ee8158007
Tim Redmond <tim.redmond@xero.com>
2019-10-31 11:35:19 +1300

    Export XUIBannerMessageDetail and release 15.2.6

--- src/react/banner.js
+++ src/react/banner.js
@@ -2,5 +2,12 @@ import XUIBanner from './components/banner/XUIBanner';
 import XUIBannerAction from './components/banner/XUIBannerAction';
 import XUIBannerActions from './components/banner/XUIBannerActions';
 import XUIBannerMessage from './components/banner/XUIBannerMessage';
+import XUIBannerMessageDetail from './components/banner/XUIBannerMessageDetail';
 
-export { XUIBanner as default, XUIBannerAction, XUIBannerActions, XUIBannerMessage };
+export {
+  XUIBanner as default,
+  XUIBannerAction,
+  XUIBannerActions,
+  XUIBannerMessage,
+  XUIBannerMessageDetail,
+};

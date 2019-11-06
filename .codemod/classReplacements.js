c1f25c6256e67c117f07fa0eaf7446bb4989edd9
Vicky Min <jiepei.min@xero.com>
2019-11-07 12:52:14 +1300

    [breaking-changes] XUI-452 Update XUIToggleOption classnames to fit our naming convention (#4475)

--- .codemod/classReplacements.js
+++ .codemod/classReplacements.js
@@ -1,6 +1,8 @@
 const classMap = {
   'xui-pageheading--breadcrumbs': 'xui-pageheading--breadcrumbtrail',
   'xui-breadcrumbs': 'xui-breadcrumbtrail',
+  'xui-toggle-option': 'xui-toggle--option',
+  'xui-toggle-optionwrapper': 'xui-toggle--optionwrapper',
 };
 
 module.exports = classMap;

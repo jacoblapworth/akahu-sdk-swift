26dff21aa54f81ec292168188bfefb0d71044b11
Nick Piesco <nick.piesco@xero.com>
2020-01-16 15:34:43 +1300

    [XUI-954] Prevent KSS sidebar panels from stacking
    We show and hide sample markup panels with an... *interesting* variation
    of the radio button CSS hack. We need to manually set the state of all
    the relevant radio buttons, closing all the sample markup panels on the
    page, before opening the correct one.
    
    I used tabs instead of spaces to keep with the existing format of the
    file, but Prettier did trim some whitespace from an earlier comment.

--- .kss/builder/kss-assets/kss-script.js
+++ .kss/builder/kss-assets/kss-script.js
@@ -1,6 +1,6 @@
-/** 
+/**
  * Bootstrap a few runtime KSS things
- * 
+ *
  * This script initalises Pretty print
  * This script initalises the Jump to nav menu
  */
@@ -16,4 +16,29 @@ document.addEventListener('DOMContentLoaded', function () {
 			document.location.hash = navSectionSelect.value;
 		});
 	}
+});
+
+/**
+ * Closes all the markup sample panels when another is opened
+ * to prevent multiple panels being open simultaneously. Counterintuitively,
+ * when `checked` is true, the panel is *not* displayed, and vice versa.
+ * Overqualifying a selector and adding some guards here
+ * because you can never be *really* sure.
+ */
+const showMarkupButtons = document.querySelectorAll('.ds-example-markuplink');
+const showMarkupRadios = document.querySelectorAll('input[type="radio"].ds-input-example');
+
+showMarkupButtons && showMarkupButtons.forEach(button => {
+	button.addEventListener('click', e => {
+		const associatedRadioId = e.target.attributes.for && e.target.attributes.for.value;
+		const associatedRadio = document.getElementById(associatedRadioId);
+
+		showMarkupRadios && showMarkupRadios.forEach(radio => {
+			radio.checked = true;
+		})
+
+		if (associatedRadio) {
+			associatedRadioId.checked = false;
+		};
+	});
 });

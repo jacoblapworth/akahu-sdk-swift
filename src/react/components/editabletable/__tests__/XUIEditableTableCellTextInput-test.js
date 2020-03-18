c68af26f8b9d1ceec91c4b1269e87f2a26f4ed76
dev-alexlapwood <alex.lapwood@xero.com>
2020-03-18 15:39:50 +1300

    Rename prop spreading unit tests

--- src/react/components/editabletable/__tests__/XUIEditableTableCellTextInput-test.js
+++ src/react/components/editabletable/__tests__/XUIEditableTableCellTextInput-test.js
@@ -15,34 +15,36 @@ describe('<XUIEditableTableCellTextInput />', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
-  it('renders with cellProps', () => {
-    const wrapper = mount(
-      <table>
-        <tbody>
-          <tr>
-            <XUIEditableTableCellTextInput cellProps={{ width: '50px' }} />
-          </tr>
-        </tbody>
-      </table>,
-    );
-    expect(
-      wrapper
-        .find(XUIEditableTableCell)
-        .getDOMNode()
-        .getAttribute('width'),
-    ).toBe('50px');
-  });
+  describe('spreading props', () => {
+    it('spreads cellProps onto the table cell', () => {
+      const wrapper = mount(
+        <table>
+          <tbody>
+            <tr>
+              <XUIEditableTableCellTextInput cellProps={{ width: '50px' }} />
+            </tr>
+          </tbody>
+        </table>,
+      );
+      expect(
+        wrapper
+          .find(XUIEditableTableCell)
+          .getDOMNode()
+          .getAttribute('width'),
+      ).toBe('50px');
+    });
 
-  it('renders with spread props of text input', () => {
-    const wrapper = mount(
-      <table>
-        <tbody>
-          <tr>
-            <XUIEditableTableCellTextInput id="testId" />
-          </tr>
-        </tbody>
-      </table>,
-    );
-    expect(wrapper.find(XUITextInput).props().id).toBe('testId');
+    it('spreads the rest of the props onto XUITextInput', () => {
+      const wrapper = mount(
+        <table>
+          <tbody>
+            <tr>
+              <XUIEditableTableCellTextInput id="testId" />
+            </tr>
+          </tbody>
+        </table>,
+      );
+      expect(wrapper.find(XUITextInput).props().id).toBe('testId');
+    });
   });
 });

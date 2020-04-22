9beb745edb18e0014e0b39a8c3103c94874ab484
James Won <james.won@xero.com>
2020-04-23 09:59:18 +1200

    [Minor][XUI-587] Convert hardcoded icon shapes to props with default shapes (#4773)
    Rationale for work: i18n. Localisation may require particular icons to be inserted.
    The default icon ensures that this will not affect users unless they specifically want to change the icon:
    
    * Remove hardcoded icon objects and add icon shape props with default icon shapes.
    
    * Add icon prop type to typescript definition files.
    
    * Ensure icon props are optional.
    
    * Surface props to relevant parent components to allow users to add prop.
    
    * Fix lint issues.
    
    * Disable jsx-ally non-interactive prop (Following pattern here: https://github.dev.xero.com/UXE/xui/pull/4264)
    Refactor to update prop names and description.
    
    * Update accordion item to use more descriptive name for icon.
    
    * Update description to be specific about brief name and icon name.
    
    * Update types.
    
    * Add missing description to types.
    
    * General refactoring of descriptions for readability.

--- src/react/components/progressindicator/XUIProgressCircular.d.ts
+++ src/react/components/progressindicator/XUIProgressCircular.d.ts
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { XUIIconData } from '../icon/XUIIcon';
 import { COLORS } from './helpers/constants';
 
 interface Props {
@@ -15,6 +16,16 @@ interface Props {
    * Content to place inside the "track" circle.
    */
   children?: React.ReactNode;
+  /**
+   * Optional prop for users to modify the completion icon, if required for localisation.
+   * A default icon is applied if not provided.
+   * */
+  completedIcon?: XUIIconData;
+  /**
+   * Optional prop for users to modify the error icon, if required for localisation.
+   * A default icon is applied if not provided.
+   * */
+  errorIcon?: XUIIconData;
   /**
    * Customise the "error" content.
    */

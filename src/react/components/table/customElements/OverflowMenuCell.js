jacoblapworth/akahu-sdk-swift

commit 32250b3c4e43ab73f50769e97062f03b555c0669

[patch] XUI-799 Update table interaction to remove package dependency (#4433)
* XUI-799 Update table interaction to remove package dependency

* Package and import updates, since we no longer need the polyfill

src/react/components/table/customElements/OverflowMenuCell.js
@@ -4,6 +4,7 @@ import cn from 'classnames';
 
 import { createCellLocationClasses, cellClassNames } from '../helpers/utilities';
 import { NAME_SPACE, NBSP } from '../helpers/constants';
+import preventDefault from '../../helpers/preventDefault';
 
 import TableData from './TableData';
 import OverflowMenu from './OverflowMenu';
@@ -23,7 +24,12 @@ export default class OverflowMenuCell extends PureComponent {
     );
 
     return (
-      <TableData className={className} tabIndex={-1}>
+      <TableData
+        className={className}
+        onClick={preventDefault}
+        onKeyPress={preventDefault}
+        tabIndex={-1}
+      >
         {NBSP}
         {hasItems && (
           <OverflowMenu overflowMenuTitle={overflowMenuTitle}>{overflowMenuItems}</OverflowMenu>

src/react/components/table/XUITable.js
@@ -1,5 +1,4 @@
 import 'core-js/features/array/fill';
-import 'element-closest';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';

src/react/components/table/customElements/CheckBoxCell.js
@@ -4,6 +4,7 @@ import cn from 'classnames';
 
 import TableData from './TableData';
 import XUICheckbox from '../../checkbox/XUICheckbox';
+import preventDefault from '../../helpers/preventDefault';
 
 import { createCellLocationClasses, cellClassNames } from '../helpers/utilities';
 import { NAME_SPACE, NBSP } from '../helpers/constants';
@@ -27,7 +28,12 @@ export default class CheckBoxCell extends PureComponent {
     );
 
     return (
-      <TableData className={className} tabIndex={-1}>
+      <TableData
+        className={className}
+        onClick={preventDefault}
+        onKeyPress={preventDefault}
+        tabIndex={-1}
+      >
         {NBSP}
         <XUICheckbox
           className={`${NAME_SPACE}--checkbox-body`}

src/react/components/table/customElements/TableBodyRow.js
@@ -21,8 +21,7 @@ class TableBodyRow extends PureComponent {
     const { onRowClick, rowData } = this.props;
     const { target, currentTarget } = event;
     const actionClassName = `${NAME_SPACE}--cell-action`;
-    const isAction =
-      target.classList.contains(`${actionClassName}`) || target.closest(`.${actionClassName}`);
+    const isAction = target.classList.contains(`${actionClassName}`) || event.defaultPrevented;
     const isValidInteraction = queryIsValidInteraction(event);
 
     if (!isAction && isValidInteraction) {

src/react/components/table/customElements/TableHead.js
@@ -11,6 +11,7 @@ import {
   queryIsValidInteraction,
 } from '../helpers/utilities';
 import { NAME_SPACE, NBSP } from '../helpers/constants';
+import preventDefault from '../../helpers/preventDefault';
 import TableData from './TableData';
 import { ns } from '../../helpers/xuiClassNamespace';
 
@@ -33,7 +34,13 @@ class CheckBoxCell extends PureComponent {
     const isIndeterminate = Boolean(totalCheckIds && !isChecked);
 
     return (
-      <TableData className={className} isHead tabIndex={-1}>
+      <TableData
+        className={className}
+        isHead
+        onClick={preventDefault}
+        onKeyPress={preventDefault}
+        tabIndex={-1}
+      >
         {NBSP}
         {onCheckAllToggle && (
           <XUICheckbox
@@ -70,7 +77,7 @@ class OverflowMenuCell extends PureComponent {
     );
 
     return (
-      <TableData className={className} isHead>
+      <TableData className={className} isHead onClick={preventDefault} onKeyPress={preventDefault}>
         {NBSP}
       </TableData>
     );

src/react/components/table/stories/stories.js
@@ -123,13 +123,16 @@ storiesWithKnobs.add('Playground', () => {
 
   const onRowClick = boolean('onRowClick', false);
   const rowClickProps = onRowClick && {
-    onRowClick: ({ _id }) => () => alert(`Click row ${_id}`),
+    onRowClick: ({ _id }) => alert(`Click row ${_id}`),
+    shouldRowClick: () => true,
   };
 
   const onCellClick = boolean('onCellClick', false);
   const hasWrapping = boolean('hasWrapping', false);
   const cellProps = {
-    ...(onCellClick && { onCellClick: ({ _id }) => () => alert(`Click cell in row ${_id}`) }),
+    ...(onCellClick && {
+      onCellClick: ({ _id }) => alert(`Click cell in row ${_id}`),
+    }),
     hasWrapping,
   };
 

commit 642dfc5a03568b9875c66bd45e5f54ee4de3e01e

[XUI-1140] Fix focus trap in table overflow menu
This fixes a bug introduced in #4433 and mostly fixed in #4441.
`preventDefault` on the table cell prevented keyboard actions from
reaching the buttons, breaking tabbing and menu behaviour.

src/react/components/table/customElements/OverflowMenuCell.js
@@ -4,7 +4,6 @@ import cn from 'classnames';
 
 import { createCellLocationClasses, cellClassNames } from '../helpers/utilities';
 import { NAME_SPACE, NBSP } from '../helpers/constants';
-import preventDefault from '../../helpers/preventDefault';
 
 import TableData from './TableData';
 import OverflowMenu from './OverflowMenu';
@@ -24,12 +23,7 @@ export default class OverflowMenuCell extends PureComponent {
     );
 
     return (
-      <TableData
-        className={className}
-        onClick={preventDefault}
-        onKeyPress={preventDefault}
-        tabIndex={-1}
-      >
+      <TableData className={className} tabIndex={-1}>
         {NBSP}
         {hasItems && (
           <OverflowMenu overflowMenuTitle={overflowMenuTitle}>{overflowMenuItems}</OverflowMenu>

src/react/components/table/__tests__/__snapshots__/table-test.js.snap
@@ -2955,12 +2955,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -3024,12 +3018,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -3093,12 +3081,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -3807,12 +3789,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -3918,12 +3894,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -4029,12 +3999,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -4347,12 +4311,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -4491,12 +4449,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -4635,12 +4587,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -5957,12 +5903,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -6059,12 +5999,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -6161,12 +6095,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -7710,12 +7638,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -7854,12 +7776,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
@@ -7998,12 +7914,6 @@ exports[`<XUITable /> emulate stories should render scenario "Instances/XUITable
               </td>
               <td
                 className="xui-table--cell-action xui-table--cell xui-table--cell-layout xui-table--cell-last xui-table--cell-divider"
-                onClick={[Function]}
-                onKeyDown={[Function]}
-                onKeyPress={[Function]}
-                onPointerOut={[Function]}
-                onPointerOver={[Function]}
-                role="button"
                 tabIndex={-1}
               >
                  
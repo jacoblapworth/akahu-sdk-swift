b866258b59ab2f44b2b7c1edbb645b007148b1ca
James Won <james.won@xero.com>
2021-11-10 14:23:44 +1300

    Revert "[minor][noRef] Sync patch -> minor (#5786) (#5787)" (#5788)
    This reverts commit 82140f9f74064709365f1cc8939b118df2ba5210.

---
+++ src/react/components/tag/XUITag.d.ts
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import { sizes, variants } from './private/constants';
+
+interface Props {
+  children?: React.ReactNode;
+  className?: string;
+  /**
+   * @ignore
+   * Dev / debug prop to show the tooltip initially on mount instead of based on a user event.
+   */
+  debugShowToolTip?: boolean;
+  /**
+   * Id for tooltip.
+   */
+  id?: string;
+  qaHook?: string;
+  /**
+   * Size of tag to render.
+   */
+  size?: keyof typeof sizes;
+  /**
+   * Type of tag to render.
+   */
+  variant?: keyof typeof variants;
+}
+
+declare const XUITag: React.FunctionComponent<Props>;
+export default XUITag;

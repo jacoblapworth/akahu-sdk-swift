9a4aa263a92194c8e771fac4d28764ab34f083d8
Alexander Lee <alexander.lee@xero.com>
2022-02-04 15:36:23 +1300

    [XUI-2531] Make xui docs version node 10 compatible (#5939)
    ESLint prompted a change from `require` to `import`, but unfortunately that is incompatible with our version of node.

--- scripts/build/docs/private/xuiDocsVersion.js
+++ scripts/build/docs/private/xuiDocsVersion.js
@@ -1,12 +1,11 @@
-import { version as xuiDocsVersion } from '../../../../package.json';
-
 /**
  * Determines the folder name for this docs build based on the version in `package.json`.
  *
  * @returns The version of XUI to build the docs for (x.x.x[-(alpha|beta).x])
  */
 function getXuiDocsVersion() {
-  return xuiDocsVersion;
+  // eslint-disable-next-line global-require
+  return require('../../../../package.json').version;
 }
 
 module.exports = getXuiDocsVersion();

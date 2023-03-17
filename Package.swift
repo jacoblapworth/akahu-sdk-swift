bfa6291a214c2fca0987bf5c932b5e2dc41013a1
J <jacob@lapworth.nz>
2023-03-17 15:23:07 +1300

    Pin swift-url-routing

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.3.0"),
     .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.2.0"),
-    .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.4.0")
+    .package(url: "https://github.com/pointfreeco/swift-url-routing", branch: "swift-5-8")
   ],
   targets: [
     .target(

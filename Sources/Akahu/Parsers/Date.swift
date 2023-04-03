c8c4d48e4c87913920d3344d72823980991c3bb0
J <jacob@lapworth.nz>
2023-04-03 13:07:58 +1200

    Break up Date parser

--- Sources/Akahu/Parsers/Date.swift
+++ Sources/Akahu/Parsers/Date.swift
@@ -8,6 +8,10 @@
 import Foundation
 import Parsing
 
+
+/// Parse a Substring into DateComponents
+///
+/// Equivalent to ISO8601DateFormatter
 struct DateTime: ParserPrinter {
   enum Errors: Error {
     case invalidDate
@@ -46,25 +50,48 @@ struct DateTime: ParserPrinter {
       Digits(2)
       ":".utf8
       Digits(2)
-      Optionally {
+      Self.nanosecond
+      Self.timeZone
+    }
+  }
+  
+  /// Parse a Substring into an Int of nanoseconds
+  static var nanosecond: some Parser<Substring, Int?> {
+    Optionally {
+      Parse {
         ".".utf8
         Prefix(1...9, while: (UInt8(ascii: "0")...UInt8(ascii: "9")).contains)
           .compactMap { n in Int(Substring(n)).map { $0 * Int(pow(10, 9 - Double(n.count))) } }
       }
+    }
+  }
+  
+  /// Parse a Substring into a TimeZone
+  ///
+  /// Z or +12:00
+  static var timeZone: some Parser<Substring.UTF8View, TimeZone> {
+    Parse(input: Substring.UTF8View.self) {
       OneOf {
         "Z".utf8.map { 0 }
-        Parse {
-          OneOf {
-            "+".utf8.map { 1 }
-            "-".utf8.map { -1 }
-          }
-          Digits(2).map { $0 * 60 }
-          ":".utf8
-          Digits(2)
-        }
-        .map { $0 * ($1 + $2) }
+        offset
       }
-      .map { TimeZone(secondsFromGMT: $0) }
+      .compactMap { TimeZone(secondsFromGMT: $0) }
     }
+  }
+  
+  /// Parse a Substring matching a UTC offset, into seconds
+  ///
+  /// ±12:00 = 43_200
+  static var offset: some Parser<Substring.UTF8View, Int> {
+    Parse(input: Substring.UTF8View.self) {
+      OneOf {
+        "+".utf8.map { 1 }
+        "-".utf8.map { -1 }
+      }
+      Digits(2).map { $0 * 60 * 60 }
+      ":".utf8
+      Digits(2).map { $0 * 60 }
+    }
+    .map { $0 * ($1 + $2) }
   }
 }

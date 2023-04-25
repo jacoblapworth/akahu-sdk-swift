70cc10fa14413ba0bc80fa06aa06d89c6a50b525
J <jacob@lapworth.nz>
2023-04-26 11:02:38 +1200

    user token auth

--- Tests/AkahuTests/ClientTests.swift
+++ Tests/AkahuTests/ClientTests.swift
@@ -26,7 +26,7 @@ class ClientTests: XCTestCase {
   
   func testAuthenticatingRouter() throws {
     if #available(iOS 16.0, macOS 13.0, *) {
-      XCTAssertThrowsError(try Akahu.shared.authenticateRouter(appToken: "123")) { error in
+      XCTAssertThrowsError(try Akahu.shared.authenticateRouter(appToken: "123", userToken: "user_token_123")) { error in
         XCTAssertTrue(
           error is Akahu.Errors,
           "Unexpected error type: \(type(of: error))"
@@ -46,11 +46,13 @@ class ClientTests: XCTestCase {
       }
     }
     
-    let router = try Akahu.shared.authenticateRouter(appToken: "app_token_123")
+    let router = try Akahu.shared.authenticateRouter(appToken: "app_token_123", userToken: "user_token_123")
     let request = try router.request(for: .me())
     
+    XCTAssertEqual(request.value(forHTTPHeaderField: "Authorization"), "Bearer user_token_123")
+    XCTAssertEqual(request.value(forHTTPHeaderField: "x-akahu-id"), "app_token_123")
     XCTAssertNoDifference(
-      #"["x-akahu-id": "app_token_123"]"#,
+      #"["Authorization": "Bearer user_token_123", "x-akahu-id": "app_token_123"]"#,
       request.allHTTPHeaderFields?.description
     )
   }

e619fe0cf9aeebfbae55d1bf24aaa57b43fa15ae
J <jacob@lapworth.nz>
2023-06-27 11:48:22 +1200

    Update connection model

--- Sources/Akahu/Models/Connection/Connection.swift
+++ Sources/Akahu/Models/Connection/Connection.swift
@@ -13,15 +13,25 @@ import Foundation
 /// Non-bank integrations are more variable because these institutions tend to offer differing functionality in their consumer apps.
 /// [Supported integrations](https://developers.akahu.nz/docs/integrations#supported-integrations)
 public struct AkahuConnection: Codable, Identifiable, Hashable {
+  /// A unique identifier for this institution, prefixed by conn_.
   public let id: String
+  /// The name of the institution.
   public let name: String
+  /// Image of the institution's logo.
   public let logo: URL
+  /// The institution's website.
+  public let url: URL? = nil
+  /// Mapping of the connection ID to known financial insitutions.
   public var bank: BankConnection? {
     BankConnection(rawValue: self.id)
   }
   
   enum CodingKeys: String, CodingKey {
     case id = "_id"
-    case name, logo
+    case name, logo, url
   }
+}
+
+extension AkahuConnection: Equatable {
+  
 }
